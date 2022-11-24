6a77a5c2253f532fc58605c657ba79fc299fb7f3
Steve Korshakov <steve@korshakov.com>
2022-11-24 13:45:44 +0400

    feat: add id to grammar items

--- src/grammar/grammar.ts
+++ src/grammar/grammar.ts
@@ -1,81 +1,87 @@
 import rawGrammar from './grammar.ohm-bundle';
 import { GrammarNodes } from './types';
 
-//
-// Implementation
-//
+// Tools
+type DistributiveOmit<T, K extends keyof any> = T extends any
+    ? Omit<T, K>
+    : never;
+let nextId = 1;
+function createNode(src: DistributiveOmit<GrammarNodes, 'id'>): GrammarNodes {
+    return Object.freeze(Object.assign({ id: nextId++ }, src));
+}
 
+// Semantics
 const semantics = rawGrammar.createSemantics();
 
 // Resolve program
 semantics.addOperation<GrammarNodes>('resolve_program', {
     Program(arg0) {
-        return {
+        return createNode({
             kind: 'program',
             entries: arg0.children.map((v) => v.resolve_program_item())
-        };
+        });
     },
 });
 
 // Resolve program items
 semantics.addOperation<GrammarNodes>('resolve_program_item', {
     Struct(arg0, arg1, arg2, arg3, arg4) {
-        return {
+        return createNode({
             kind: 'def_struct',
             name: arg1.sourceString,
             fields: arg3.children.map((v) => v.resolve_declaration())
-        }
+        })
     },
     Contract(arg0, arg1, arg2, arg3, arg4) {
-        return {
+        return createNode({
             kind: 'def_contract',
             name: arg1.sourceString,
             declarations: arg3.children.map((v) => v.resolve_declaration())
-        }
+        })
     },
 });
 
 // Struct and class declarations
 semantics.addOperation<GrammarNodes>('resolve_declaration', {
     Field(arg0, arg1, arg2, arg3, arg4) {
-        return {
+        return createNode({
             kind: 'def_field',
             name: arg1.sourceString,
             type: arg3.sourceString
-        }
+        })
     },
     FunctionArg(arg0, arg1, arg2) {
-        return {
+        return createNode({
             kind: 'def_argument',
             name: arg0.sourceString,
             type: arg2.sourceString
-        }
+        })
     },
     Function(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) {
-        return {
+        return createNode({
             kind: 'def_function',
             name: arg1.sourceString,
             return: arg6.sourceString,
             args: arg3.asIteration().children.map((v: any) => v.resolve_declaration()),
             statements: arg8.children.map((v: any) => v.resolve_statement()),
-        }
+        })
     },
 });
 
 // Statements
 semantics.addOperation<GrammarNodes>('resolve_statement', {
     StatementLet(arg0, arg1, arg2, arg3, arg4) {
-        return {
+        return createNode({
             kind: 'let',
             name: arg1.sourceString,
             expression: arg3.resolve_expression()
-        }
+        })
     },
     StatementReturn(arg0, arg1, arg2) {
-        return {
+        return createNode({
             kind: 'return',
             expression: arg1.resolve_expression()
-        }
+        })
     },
 });
 
@@ -83,34 +89,34 @@ semantics.addOperation<GrammarNodes>('resolve_statement', {
 semantics.addOperation<GrammarNodes>('resolve_expression', {
     integerLiteral(n) {
         // Parses dec-based integer and hex-based integers
-        return ({ kind: 'number', value: BigInt(n.sourceString) });
+        return createNode({ kind: 'number', value: BigInt(n.sourceString) });
     },
     id(arg0, arg1) {
-        return ({ kind: 'id', value: arg0.sourceString + arg1.sourceString });
+        return createNode({ kind: 'id', value: arg0.sourceString + arg1.sourceString });
     },
     nullLiteral(arg0) {
-        return ({ kind: 'null' });
+        return createNode({ kind: 'null' });
     },
     boolLiteral(arg0) {
-        return ({ kind: 'boolean', value: arg0.sourceString === 'true' });
+        return createNode({ kind: 'boolean', value: arg0.sourceString === 'true' });
     },
     ExpressionAdd_add(arg0, arg1, arg2) {
-        return ({ kind: 'op_binary', op: '+', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
+        return createNode({ kind: 'op_binary', op: '+', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
     },
     ExpressionAdd_sub(arg0, arg1, arg2) {
-        return ({ kind: 'op_binary', op: '-', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
+        return createNode({ kind: 'op_binary', op: '-', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
     },
     ExpressionMul_div(arg0, arg1, arg2) {
-        return ({ kind: 'op_binary', op: '/', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
+        return createNode({ kind: 'op_binary', op: '/', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
     },
     ExpressionMul_mul(arg0, arg1, arg2) {
-        return ({ kind: 'op_binary', op: '*', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
+        return createNode({ kind: 'op_binary', op: '*', left: arg0.resolve_expression(), right: arg2.resolve_expression() });
     },
     ExpressionField(arg0, arg1, arg2) {
-        return ({ kind: 'op_field', src: arg0.resolve_expression(), key: arg2.sourceString });
+        return createNode({ kind: 'op_field', src: arg0.resolve_expression(), key: arg2.sourceString });
     },
     ExpressionCall(arg0, arg1, arg2, arg3, arg4, arg5) {
-        return ({ kind: 'op_call', src: arg0.resolve_expression(), key: arg2.sourceString, args: arg4.asIteration().children.map((v: any) => v.resolve_expression()) });
+        return createNode({ kind: 'op_call', src: arg0.resolve_expression(), key: arg2.sourceString, args: arg4.asIteration().children.map((v: any) => v.resolve_expression()) });
     },
 });
 
--- src/grammar/types.ts
+++ src/grammar/types.ts
@@ -4,20 +4,24 @@
 
 export type GrammarNumber = {
     kind: 'number',
+    id: number,
     value: bigint
 }
 
 export type GrammarID = {
     kind: 'id',
+    id: number,
     value: string
 }
 
 export type GrammarNull = {
     kind: 'null'
+    id: number,
 }
 
 export type GrammarBoolean = {
     kind: 'boolean',
+    id: number,
     value: boolean
 }
 
@@ -27,6 +31,7 @@ export type GrammarBoolean = {
 
 export type GrammarOpBinary = {
     kind: 'op_binary',
+    id: number,
     op: '+' | '-' | '*' | '/',
     left: GrammarExpression,
     right: GrammarExpression,
@@ -34,18 +39,21 @@ export type GrammarOpBinary = {
 
 export type GrammarOpUnary = {
     kind: 'op_unary',
+    id: number,
     op: '+' | '-' | '!' | '~',
     right: GrammarExpression,
 }
 
 export type GrammarOpField = {
     kind: 'op_field'
+    id: number,
     src: GrammarExpression,
     key: string
 }
 
 export type GrammarOpCall = {
     kind: 'op_call'
+    id: number,
     src: GrammarExpression,
     key: string,
     args: GrammarExpression[]
@@ -57,35 +65,41 @@ export type GrammarOpCall = {
 
 export type GrammarProgram = {
     kind: 'program',
+    id: number,
     entries: (GrammarStruct | GrammarContract)[]
 }
 
 export type GrammarStruct = {
     kind: 'def_struct',
+    id: number,
     name: string
     fields: GrammarField[]
 }
 
 export type GrammarField = {
     kind: 'def_field',
+    id: number,
     name: string,
     type: string
 }
 
 export type GrammarContract = {
     kind: 'def_contract',
+    id: number,
     name: string,
     declarations: (GrammarField)[]
 }
 
 export type GrammarArgument = {
     kind: 'def_argument',
+    id: number,
     name: string,
     type: string
 }
 
 export type GrammarFunction = {
     kind: 'def_function',
+    id: number,
     name: string,
     return: string,
     args: GrammarArgument[],
@@ -98,12 +112,14 @@ export type GrammarFunction = {
 
 export type GrammarStatementLet = {
     kind: 'let',
+    id: number,
     name: string,
     expression: GrammarExpression
 }
 
 export type GrammarStatementReturn = {
     kind: 'return',
+    id: number,
     expression: GrammarExpression
 }
 
@@ -113,4 +129,4 @@ export type GrammarStatementReturn = {
 
 export type GrammarStatement = GrammarStatementLet | GrammarStatementReturn;
 export type GrammarExpression = GrammarOpBinary | GrammarOpUnary | GrammarOpField | GrammarNumber | GrammarNull | GrammarID | GrammarBoolean;
-export type GrammarNodes = GrammarExpression | GrammarProgram | GrammarStruct | GrammarField | GrammarContract | GrammarArgument | GrammarFunction | GrammarOpCall | GrammarStatementLet | GrammarStatementReturn;
+export type GrammarNodes = GrammarExpression | GrammarProgram | GrammarStruct | GrammarField | GrammarContract | GrammarArgument | GrammarFunction | GrammarOpCall | GrammarStatementLet | GrammarStatementReturn | GrammarProgram;
