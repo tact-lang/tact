e72b98b8af037f901ff677c39b5a27d2cd91e1e1
Steve Korshakov <steve@korshakov.com>
2022-12-13 14:24:26 +0400

    wip: working on multisig contract

---
+++ src/examples/multisig.Multisig.bind.ts
@@ -0,0 +1,125 @@
+import { Cell, Slice, StackItem, Address, Builder, InternalMessage, CommonMessageInfo, CellMessage, beginCell, serializeDict } from 'ton';
+import { ContractExecutor } from 'ton-nodejs';
+import BN from 'bn.js';
+import { deploy } from '../abi/deploy';
+
+export type SendParameters = {
+    $$type: 'SendParameters';
+    bounce: boolean;
+    to: Address;
+    value: BigInt;
+    mode: BigInt;
+    body: Cell | null;
+    code: Cell | null;
+    data: Cell | null;
+}
+
+export function packSendParameters(src: SendParameters): Cell {
+    let b_0 = new Builder();
+    b_0 = b_0.storeBit(src.bounce);
+    b_0 = b_0.storeAddress(src.to);
+    b_0 = b_0.storeInt(new BN(src.value.toString(10), 10), 257);
+    b_0 = b_0.storeInt(new BN(src.mode.toString(10), 10), 257);
+    if (src.body !== null) {
+        b_0 = b_0.storeBit(true);
+        b_0 = b_0.storeRef(src.body);
+    } else {
+        b_0 = b_0.storeBit(false);
+    }
+    if (src.code !== null) {
+        b_0 = b_0.storeBit(true);
+        b_0 = b_0.storeRef(src.code);
+    } else {
+        b_0 = b_0.storeBit(false);
+    }
+    if (src.data !== null) {
+        b_0 = b_0.storeBit(true);
+        b_0 = b_0.storeRef(src.data);
+    } else {
+        b_0 = b_0.storeBit(false);
+    }
+    return b_0.endCell();
+}
+
+export type Context = {
+    $$type: 'Context';
+    bounced: boolean;
+    sender: Address;
+    value: BigInt;
+}
+
+export function packContext(src: Context): Cell {
+    let b_0 = new Builder();
+    b_0 = b_0.storeBit(src.bounced);
+    b_0 = b_0.storeAddress(src.sender);
+    b_0 = b_0.storeInt(new BN(src.value.toString(10), 10), 257);
+    return b_0.endCell();
+}
+
+export type StateInit = {
+    $$type: 'StateInit';
+    code: Cell;
+    data: Cell;
+}
+
+export function packStateInit(src: StateInit): Cell {
+    let b_0 = new Builder();
+    b_0 = b_0.storeRef(src.code);
+    b_0 = b_0.storeRef(src.data);
+    return b_0.endCell();
+}
+
+export type Request = {
+    $$type: 'Request';
+    to: Address;
+    amount: BigInt;
+    body: Cell | null;
+}
+
+export function packRequest(src: Request): Cell {
+    let b_0 = new Builder();
+    b_0 = b_0.storeUint(4096439811, 32);
+    b_0 = b_0.storeAddress(src.to);
+    b_0 = b_0.storeInt(new BN(src.amount.toString(10), 10), 257);
+    if (src.body !== null) {
+        b_0 = b_0.storeBit(true);
+        b_0 = b_0.storeRef(src.body);
+    } else {
+        b_0 = b_0.storeBit(false);
+    }
+    return b_0.endCell();
+}
+
+export function Multisig_init(members: Cell, requiredWeight: BigInt) {
+    const __code = 'te6ccgEBCQEA6gABFP8A9KQT9LzyyAsBAgFiAgMCAswEBQAJoea/4BEBb9uBDrpOEPypgQa4WP7wFoaYGAuNhgAMi/yLhxAP0gGCogireB/DCBSK3wQQh6FVsB3XGBGHlgMkBgIB1AcIAMrtRNDUAfhigQEB1wD0BIEBAdcAgQEB1wBVMGwUBNMfAYIQ9Cq2A7ry4GT6QAEBgQEB1wBtAdIAAZLUMd5VIDMQVhBFEDRY8AnI+EIBzFUwUDSBAQHPAPQAgQEBzwCBAQHPAMntVAA7G1wBMjMUERDE1A0gQEBzwD0AIEBAc8AgQEBzwDJgAAUXwOA=';
+    const depends = new Map<string, Cell>();
+    let systemCell = beginCell().storeDict(null).endCell();
+    let __stack: StackItem[] = [];
+    __stack.push({ type: 'cell', cell: systemCell });
+    __stack.push({ type: 'cell', cell: members});
+    __stack.push({ type: 'int', value: new BN(requiredWeight.toString(), 10) });
+    return deploy(__code, 'init_Multisig', __stack); 
+}
+
+export class Multisig {
+            
+    readonly executor: ContractExecutor; 
+    constructor(executor: ContractExecutor) { this.executor = executor; } 
+    
+    async send(args: { amount: BN, from?: Address, debug?: boolean }, message: Request) {
+        let body: Cell | null = null;
+        if (message && typeof message === 'object' && !(message instanceof Slice) && message.$$type === 'Request') {
+            body = packRequest(message);
+        }
+        if (body === null) { throw new Error('Invalid message type'); }
+        await this.executor.internal(new InternalMessage({
+            to: this.executor.address,
+            from: args.from || this.executor.address,
+            bounce: false,
+            value: args.amount,
+            body: new CommonMessageInfo({
+                body: new CellMessage(body!)
+            })
+        }), { debug: args.debug });
+    }
+}
