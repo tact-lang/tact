3e74dc5c45c9ceb33ad9af0dc7a38a6ff8fb7374
Steve Korshakov <steve@korshakov.com>
2022-12-16 01:37:26 +0400

    ref: update snapshots

--- src/test/integration.spec.ts
+++ src/test/integration.spec.ts
@@ -9,12 +9,12 @@ describe('integration', () => {
         __DANGER_resetNodeId();
     });
     for (let r of loadCases(__dirname + "/contracts/")) {
-        it('should resolve expressions for ' + r.name, () => {
+        it('should resolve expressions for ' + r.name, async () => {
             let ctx = new CompilerContext({ shared: {} });
             ctx = precompile(ctx, __dirname + "/contracts/" + r.name + '.tact');
             let contract = getContracts(ctx)[0];
-            let res = compile(ctx, contract);
-            expect(res.output).toEqual(fs.readFileSync(__dirname + "/contracts/" + r.name + '.' + contract + '.fc', 'utf8'));
+            let res = await compile(ctx, contract);
+            expect(res.output.output).toEqual(fs.readFileSync(__dirname + "/contracts/" + r.name + '.' + contract + '.fc', 'utf8'));
         });
     }
 });
