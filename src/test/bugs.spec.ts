717226cf517753c95853d95990bb06797a298ff1
Steve Korshakov <steve@korshakov.com>
2023-01-04 11:04:42 +0400

    test: add bug#18 test

---
+++ src/test/bugs.spec.ts
@@ -0,0 +1,15 @@
+import { toNano } from "ton-core";
+import { ContractSystem, testAddress } from "ton-emulator";
+import { DDDD } from './bugs/output/bug_18_DDDD';
+
+describe('bugs', () => {
+    it('not have a bug #18', async () => {
+        let system = await ContractSystem.create();
+        let treasure = system.treasure('treasure');
+        let contract = system.open(await DDDD.fromInit(testAddress('1'), testAddress('3'), testAddress('2')));
+        let tracker = system.track(contract.address);
+        await contract.send(treasure, { value: toNano('10') }, null);
+        await system.run();
+        expect(tracker.events()).toMatchSnapshot();
+    });
+});
---
+++ src/test/bugs/output/bug_18_DDDD.ts
@@ -0,0 +1,280 @@
+import { Cell, Slice, Address, Builder, beginCell, ComputeError, TupleItem, TupleReader, Dictionary, contractAddress, ContractProvider, Sender, Contract, ContractABI } from 'ton-core';
+import { ContractSystem, ContractExecutor } from 'ton-emulator';
+
+export type StateInit = {
+    $$type: 'StateInit';
+    code: Cell;
+    data: Cell;
+}
+
+export function storeStateInit(src: StateInit) {
+    return (builder: Builder) => {
+        let b_0 = builder;
+        b_0.storeRef(src.code);
+        b_0.storeRef(src.data);
+    };
+}
+
+export function loadStateInit(slice: Slice) {
+    let sc_0 = slice;
+    let _code = sc_0.loadRef();
+    let _data = sc_0.loadRef();
+    return { $$type: 'StateInit' as const, code: _code, data: _data };
+}
+
+function loadTupleStateInit(source: TupleReader) {
+    const _code = source.readCell();
+    const _data = source.readCell();
+    return { $$type: 'StateInit' as const, code: _code, data: _data };
+}
+
+function storeTupleStateInit(source: StateInit) {
+    let __tuple: TupleItem[] = [];
+    __tuple.push({ type: 'cell', cell: source.code });
+    __tuple.push({ type: 'cell', cell: source.data });
+    return __tuple;
+}
+
+export type Context = {
+    $$type: 'Context';
+    bounced: boolean;
+    sender: Address;
+    value: bigint;
+    raw: Cell;
+}
+
+export function storeContext(src: Context) {
+    return (builder: Builder) => {
+        let b_0 = builder;
+        b_0.storeBit(src.bounced);
+        b_0.storeAddress(src.sender);
+        b_0.storeInt(src.value, 257);
+        b_0.storeRef(src.raw);
+    };
+}
+
+export function loadContext(slice: Slice) {
+    let sc_0 = slice;
+    let _bounced = sc_0.loadBit();
+    let _sender = sc_0.loadAddress();
+    let _value = sc_0.loadIntBig(257);
+    let _raw = sc_0.loadRef();
+    return { $$type: 'Context' as const, bounced: _bounced, sender: _sender, value: _value, raw: _raw };
+}
+
+function loadTupleContext(source: TupleReader) {
+    const _bounced = source.readBoolean();
+    const _sender = source.readAddress();
+    const _value = source.readBigNumber();
+    const _raw = source.readCell();
+    return { $$type: 'Context' as const, bounced: _bounced, sender: _sender, value: _value, raw: _raw };
+}
+
+function storeTupleContext(source: Context) {
+    let __tuple: TupleItem[] = [];
+    __tuple.push({ type: 'int', value: source.bounced ? -1n : 0n });
+    __tuple.push({ type: 'slice', cell: beginCell().storeAddress(source.sender).endCell() });
+    __tuple.push({ type: 'int', value: source.value });
+    __tuple.push({ type: 'slice', cell: source.raw });
+    return __tuple;
+}
+
+export type SendParameters = {
+    $$type: 'SendParameters';
+    bounce: boolean;
+    to: Address;
+    value: bigint;
+    mode: bigint;
+    body: Cell | null;
+    code: Cell | null;
+    data: Cell | null;
+}
+
+export function storeSendParameters(src: SendParameters) {
+    return (builder: Builder) => {
+        let b_0 = builder;
+        b_0.storeBit(src.bounce);
+        b_0.storeAddress(src.to);
+        b_0.storeInt(src.value, 257);
+        b_0.storeInt(src.mode, 257);
+        if (src.body !== null) {
+            b_0.storeBit(true);
+            b_0.storeRef(src.body);
+        } else {
+            b_0.storeBit(false);
+        }
+        if (src.code !== null) {
+            b_0.storeBit(true);
+            b_0.storeRef(src.code);
+        } else {
+            b_0.storeBit(false);
+        }
+        if (src.data !== null) {
+            b_0.storeBit(true);
+            b_0.storeRef(src.data);
+        } else {
+            b_0.storeBit(false);
+        }
+    };
+}
+
+export function loadSendParameters(slice: Slice) {
+    let sc_0 = slice;
+    let _bounce = sc_0.loadBit();
+    let _to = sc_0.loadAddress();
+    let _value = sc_0.loadIntBig(257);
+    let _mode = sc_0.loadIntBig(257);
+    let _body: Cell | null = null;
+    if (sc_0.loadBit()) {
+        _body = sc_0.loadRef();
+    }
+    let _code: Cell | null = null;
+    if (sc_0.loadBit()) {
+        _code = sc_0.loadRef();
+    }
+    let _data: Cell | null = null;
+    if (sc_0.loadBit()) {
+        _data = sc_0.loadRef();
+    }
+    return { $$type: 'SendParameters' as const, bounce: _bounce, to: _to, value: _value, mode: _mode, body: _body, code: _code, data: _data };
+}
+
+function loadTupleSendParameters(source: TupleReader) {
+    const _bounce = source.readBoolean();
+    const _to = source.readAddress();
+    const _value = source.readBigNumber();
+    const _mode = source.readBigNumber();
+    const _body = source.readCellOpt();
+    const _code = source.readCellOpt();
+    const _data = source.readCellOpt();
+    return { $$type: 'SendParameters' as const, bounce: _bounce, to: _to, value: _value, mode: _mode, body: _body, code: _code, data: _data };
+}
+
+function storeTupleSendParameters(source: SendParameters) {
+    let __tuple: TupleItem[] = [];
+    __tuple.push({ type: 'int', value: source.bounce ? -1n : 0n });
+    __tuple.push({ type: 'slice', cell: beginCell().storeAddress(source.to).endCell() });
+    __tuple.push({ type: 'int', value: source.value });
+    __tuple.push({ type: 'int', value: source.mode });
+    if (source.body !== null) {
+        __tuple.push({ type: 'cell', cell: source.body });
+    } else {
+        __tuple.push({ type: 'null' });
+    }
+    if (source.code !== null) {
+        __tuple.push({ type: 'cell', cell: source.code });
+    } else {
+        __tuple.push({ type: 'null' });
+    }
+    if (source.data !== null) {
+        __tuple.push({ type: 'cell', cell: source.data });
+    } else {
+        __tuple.push({ type: 'null' });
+    }
+    return __tuple;
+}
+
+async function DDDD_init(addr1: Address, addr2: Address, addr3: Address) {
+    const __code = 'te6ccgECGgEAAe0AART/APSkE/S88sgLAQIBYgIDAgLMBAUCASAUFQIBIAYHAgFYDg8A29OBDrpOEPypgQa4WP7wFoaYGAuNhgAMi/yLhxAP0gESgzN4J8MIFIrfBgAADrpOCQ2EcbdqJoagD8MX0gAID9IACA/SAAgPoCegIqoDYK+AfkfCEA5iqgKCpniyxniwDniwl6AHoAZPaqcHlgQUAgEgCAkCASAKCwIBIAwNABsIG6VMFn0WjDgQTP0FIAARFn0DG+h3DBtgACMIW6VW1n0WTDgyAHPAEEz9EGAAHRBM/QKb6GUAdcAMOBbbYAIBIBARAgEgEhMALRtbQXIzAVQVM8WWM8WAc8WEvQA9ADJgABMMTIzgQEBMvAFgABcbEKBAQsBgQEB8AeAAjQBgQEBcCbwBIEBAXEl8ASBAQFyJPAEgQEBcybwBAGBAQslcIEBAfAGgQELJHGBAQHwBoEBCyNygQEB8AaBAQslc4EBAfAGgAgFIFhcCASAYGQBDtFPdqJoagD8MX0gAID9IACA/SAAgPoCegIqoDYKqoJ4B0AAJtcceAZAATbu9GCcFzsPV0srnsehOw51kqFG2aCcJ3WNS0rZHyzItOvLf3xYjmABDuVw+1E0NQB+GL6QAEB+kABAfpAAQH0BPQEVUBsFVUE8A2A==';
+    const depends = Dictionary.empty(Dictionary.Keys.Uint(16), Dictionary.Values.Cell());
+    let systemCell = beginCell().storeDict(depends).endCell();
+    let __tuple: TupleItem[] = [];
+    __tuple.push({ type: 'cell', cell: systemCell });
+    __tuple.push({ type: 'slice', cell: beginCell().storeAddress(addr1).endCell() });
+    __tuple.push({ type: 'slice', cell: beginCell().storeAddress(addr2).endCell() });
+    __tuple.push({ type: 'slice', cell: beginCell().storeAddress(addr3).endCell() });
+    let codeCell = Cell.fromBoc(Buffer.from(__code, 'base64'))[0];
+    let system = await ContractSystem.create();
+    let executor = await ContractExecutor.create({ code: codeCell, data: new Cell() }, system);
+    let res = await executor.get('init_DDDD', __tuple);
+    if (!res.success) { throw Error(res.error); }
+    if (res.exitCode !== 0 && res.exitCode !== 1) {
+        if (DDDD_errors[res.exitCode]) {
+            throw new ComputeError(DDDD_errors[res.exitCode].message, res.exitCode, { logs: res.vmLogs });
+        } else {
+            throw new ComputeError('Exit code: ' + res.exitCode, res.exitCode, { logs: res.vmLogs });
+        }
+    }
+    
+    let data = res.stack.readCell();
+    return { code: codeCell, data };
+}
+
+const DDDD_errors: { [key: number]: { message: string } } = {
+    2: { message: `Stack undeflow` },
+    3: { message: `Stack overflow` },
+    4: { message: `Integer overflow` },
+    5: { message: `Integer out of expected range` },
+    6: { message: `Invalid opcode` },
+    7: { message: `Type check error` },
+    8: { message: `Cell overflow` },
+    9: { message: `Cell underflow` },
+    10: { message: `Dictionary error` },
+    13: { message: `Out of gas error` },
+    32: { message: `Method ID not found` },
+    34: { message: `Action is invalid or not supported` },
+    37: { message: `Not enough TON` },
+    38: { message: `Not enough extra-currencies` },
+    128: { message: `Null reference exception` },
+    129: { message: `Invalid serialization prefix` },
+    130: { message: `Invalid incoming message` },
+    131: { message: `Constraints error` },
+    132: { message: `Access denied` },
+    133: { message: `Contract stopped` },
+    134: { message: `Invalid argument` },
+}
+
+export class DDDD implements Contract {
+    
+    static async init(addr1: Address, addr2: Address, addr3: Address) {
+        return await DDDD_init(addr1,addr2,addr3);
+    }
+    
+    static async fromInit(addr1: Address, addr2: Address, addr3: Address) {
+        const init = await DDDD_init(addr1,addr2,addr3);
+        const address = contractAddress(0, init);
+        return new DDDD(address, init);
+    }
+    
+    static fromAddress(address: Address) {
+        return new DDDD(address);
+    }
+    
+    readonly address: Address; 
+    readonly init?: { code: Cell, data: Cell };
+    readonly abi: ContractABI = {
+        errors: DDDD_errors
+    };
+    
+    private constructor(address: Address, init?: { code: Cell, data: Cell }) {
+        this.address = address;
+        this.init = init;
+    }
+    
+    async send(provider: ContractProvider, via: Sender, args: { value: bigint, bounce?: boolean| null | undefined }, message: null) {
+        
+        let body: Cell | null = null;
+        if (message === null) {
+            body = new Cell();
+        }
+        if (body === null) { throw new Error('Invalid message type'); }
+        
+        await provider.internal(via, { ...args, body: body });
+        
+    }
+    
+    async getMapDataAddr1(provider: ContractProvider, key: bigint) {
+        let __tuple: TupleItem[] = [];
+        __tuple.push({ type: 'int', value: key });
+        let result = await provider.get('mapData_addr1', __tuple);
+        return result.stack.readAddressOpt();
+    }
+    
+    async getMapData2Addr1(provider: ContractProvider, key: Address) {
+        let __tuple: TupleItem[] = [];
+        __tuple.push({ type: 'slice', cell: beginCell().storeAddress(key).endCell() });
+        let result = await provider.get('mapData2_addr1', __tuple);
+        return result.stack.readBigNumberOpt();
+    }
+    
+}
