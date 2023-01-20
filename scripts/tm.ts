16e44bbf6fb5d91b4de9f0abe54041f5a1f07111
Steve Korshakov <steve@korshakov.com>
2023-01-20 17:02:45 +0400

    wip: working on better TM grammar

--- scripts/tm.ts
+++ scripts/tm.ts
@@ -1,9 +1,10 @@
-import { getHighlighter, BUNDLED_LANGUAGES } from 'shiki';
+import { getHighlighter, BUNDLED_LANGUAGES, BUNDLED_THEMES } from 'shiki';
 import fs from 'fs';
 const grammarTact = JSON.parse(fs.readFileSync(require.resolve('../grammar/tact.tmLanguage.json'), 'utf-8'));
 const grammarSample = fs.readFileSync(require.resolve('../grammar/sample.tact'), 'utf-8');
 (async () => {
     let highlighter = await getHighlighter({
+        themes: BUNDLED_THEMES,
         langs: [
             ...BUNDLED_LANGUAGES,
             {
@@ -14,7 +15,10 @@ const grammarSample = fs.readFileSync(require.resolve('../grammar/sample.tact'),
             }
         ] as any,
     });
-    let res = highlighter.codeToHtml(grammarSample, { lang: 'tact' });
+    let res = highlighter.codeToHtml(grammarSample, { lang: 'tact', theme: 'dark-plus' });
     res = `<html><head><meta charset="utf-8"></head><body>${res}</body></html>`;
     fs.writeFileSync(require.resolve('../grammar/sample.html'), res);
+
+    let tokens = highlighter.codeToThemedTokens(grammarSample, 'tact');
+    fs.writeFileSync(require.resolve('../grammar/sample.json'), JSON.stringify(tokens, null, 2));
 })();
