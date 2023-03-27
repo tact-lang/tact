67e15e73c3fd876f88f6c7884e17ce5b00a45e45
Steve Korshakov <steve@korshakov.com>
2023-03-27 22:42:44 +0100

    chore: upgrade code coverage tool

--- jest.teardown.js
+++ jest.teardown.js
@@ -2,6 +2,8 @@ const coverage = require('@tact-lang/coverage');
 const path = require('path');
 
 module.exports = async () => {
-    coverage.completeCoverage(path.resolve(__dirname, 'examples', 'output'));
-    coverage.completeCoverage(path.resolve(__dirname, 'src', 'test', 'features', 'output'));
+    coverage.completeCoverage([
+        path.resolve(__dirname, 'examples', 'output', '*.boc'),
+        path.resolve(__dirname, 'src', 'test', 'features', 'output', '*.boc')
+    ]);
 };
