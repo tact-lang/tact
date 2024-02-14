353b58519a09869e3957fd335785b351b74abfa8
Gusarich <danielsedovzzz@gmail.com>
2024-02-14 15:20:59 +0300

    rework number literals grammar and add more test cases

--- src/grammar/grammar.ohm-bundle.d.ts
+++ src/grammar/grammar.ohm-bundle.d.ts
@@ -141,12 +141,13 @@ export interface TactActionDict<T> extends ActionDict<T> {
   typeLiteral?: (this: NonterminalNode, arg0: NonterminalNode, arg1: IterationNode) => T;
   typeLiteralPart?: (this: NonterminalNode, arg0: NonterminalNode | TerminalNode) => T;
   integerLiteral?: (this: NonterminalNode, arg0: NonterminalNode) => T;
-  integerLiteralDec?: (this: NonterminalNode, arg0: IterationNode, arg1: IterationNode) => T;
-  integerLiteralHex?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode) => T;
-  integerLiteralBin?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode) => T;
-  integerLiteralOct?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode) => T;
+  integerLiteralDec?: (this: NonterminalNode, arg0: NonterminalNode | TerminalNode, arg1: IterationNode, arg2: IterationNode, arg3: IterationNode) => T;
+  integerLiteralHex?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode, arg3: IterationNode) => T;
+  integerLiteralBin?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode, arg3: IterationNode) => T;
+  integerLiteralOct?: (this: NonterminalNode, arg0: TerminalNode, arg1: IterationNode, arg2: IterationNode, arg3: IterationNode) => T;
   binDigit?: (this: NonterminalNode, arg0: TerminalNode) => T;
   octDigit?: (this: NonterminalNode, arg0: TerminalNode) => T;
+  nonZeroDigit?: (this: NonterminalNode, arg0: TerminalNode) => T;
   letterAsciiLC?: (this: NonterminalNode, arg0: TerminalNode) => T;
   letterAsciiUC?: (this: NonterminalNode, arg0: TerminalNode) => T;
   letterAscii?: (this: NonterminalNode, arg0: NonterminalNode) => T;
