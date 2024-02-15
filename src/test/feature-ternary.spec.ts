60788608097cae2051016d2bafe0531555d533f5
Anton Trunov <anton@ton.org>
2024-02-15 16:29:16 +0400

    some tests and fix for the grammar
    basically, we need the `? :`-operator to be right associative

--- src/test/feature-ternary.spec.ts
+++ src/test/feature-ternary.spec.ts
@@ -30,5 +30,22 @@ describe('feature-ternary', () => {
         expect(await contract.getTest4(123n, 5n)).toEqual(2n);
         expect(await contract.getTest4(5n, 789n)).toEqual(3n);
         expect(await contract.getTest4(5n, 5n)).toEqual(4n);
+
+        expect(await contract.getTest5()).toEqual(1n);
+
+        expect(await contract.getTest6()).toEqual(1n);
+
+        expect(await contract.getTest7()).toEqual(2n);
+
+        expect(await contract.getTest8()).toEqual(3n);
+
+        expect(await contract.getTest9()).toEqual(3n);
+
+        expect(await contract.getTest10(1n)).toEqual(42n);
+        expect(await contract.getTest10(2n)).toEqual(43n);
+        expect(await contract.getTest10(3n)).toEqual(44n);
+        expect(await contract.getTest10(4n)).toEqual(45n);
+        expect(await contract.getTest10(0n)).toEqual(45n);
+        expect(await contract.getTest10(42n)).toEqual(45n);
     });
 });
