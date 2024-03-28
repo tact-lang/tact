06bcc56bced4111fd0289f28d07c1dbc4d282b7b
byakuren-hijiri <159621697+byakuren-hijiri@users.noreply.github.com>
2024-03-28 16:19:02 -0300

    chore(ast, ci): remove duplicates from `ASTNode` & add CI check (#204)
    * ci[eslint]: add no-duplicate-type-constituents check
    
    Adding this check requires specifying `parserOptions.project`,
    which in its turn requires a custom `tsconfig.json` so ESLint works correctly
    
    ---------
    Co-authored-by: Anton Trunov <anton@ton.org>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    project: "./tsconfig.eslint.json",
+  },
+  ignorePatterns: ["*.cjs"],
   plugins: ['@typescript-eslint'],
   root: true,
   env: {
@@ -22,5 +28,6 @@ module.exports = {
         allow: ['/package\\.json$']
       }
     ],
+    "@typescript-eslint/no-duplicate-type-constituents": "error",
   },
 };
