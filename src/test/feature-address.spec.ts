2e5b9688fb8f5ea5740e7a0351b310f20bcca6e3
Gusarich <danielsedovzzz@gmail.com>
2024-04-10 12:41:14 +0300

    test `address()` built-in function

---
+++ src/test/feature-address.spec.ts
@@ -0,0 +1,23 @@
+import { Address, beginCell, toNano } from "@ton/core";
+import { ContractSystem } from "@tact-lang/emulator";
+import { __DANGER_resetNodeId } from "../grammar/ast";
+import { AddressTester } from "./features/output/address_AddressTester";
+
+describe("feature-address", () => {
+    beforeEach(() => {
+        __DANGER_resetNodeId();
+    });
+    it("should implement addresses correctly", async () => {
+        // Init
+        const system = await ContractSystem.create();
+        const treasure = system.treasure("treasure");
+        const contract = system.open(await AddressTester.fromInit());
+        await contract.send(treasure, { value: toNano("10") }, null);
+        await system.run();
+
+        // Check methods
+        expect((await contract.getTest1()).toRawString()).toEqual('0:4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873')
+        expect((await contract.getTest2()).toRawString()).toEqual('0:4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873')
+        expect((await contract.getTest3()).toRawString()).toEqual('0:4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873')
+    });
+});
