ad0660b73635274ece1a62a6590fe28920a263df
heli0dus <muromskiy2000@mail.ru>
2024-05-31 13:55:17 +0300

    (testing) test refactor + AST equality test

--- src/test/formatter.spec.ts
+++ src/test/formatter.spec.ts
@@ -1,24 +1,35 @@
 import fs from "fs";
-import { CompilerContext } from "../context";
 import { __DANGER_resetNodeId } from "../grammar/ast";
-import { loadCases } from "../utils/loadCases";
-import { precompile } from "../pipeline/precompile";
-import { createNodeFileSystem } from "../vfs/createNodeFileSystem";
-import { createVirtualFileSystem } from "../vfs/createVirtualFileSystem";
-import files from "../imports/stdlib";
-import { getRawAST, parsePrograms } from "../grammar/store";
 import {formatAST} from "../formatter"
 import { parse } from "../grammar/grammar";
+import {join} from 'path';
+import JSONBig from 'json-bigint'
 
 describe("formatter", () => {
-    it.each(fs.readdirSync(__dirname + "/formatting/proper/"))
+    it.each(fs.readdirSync(join(__dirname, "formatting" , "proper")))
     ("shouldn't change proper formatting", 
         (file) =>{
-
-            const filePath = __dirname + "/formatting/proper/" + file
+            const filePath = join(__dirname, "formatting" , "proper", file)
             const src = fs.readFileSync(filePath, "utf-8")
             const ast = parse(src, filePath, "user")
             const formatted = formatAST(ast)
             expect(formatted).toEqual(src)
+    })
+    it.each(fs.readdirSync(__dirname + "/formatting/proper/"))
+    ("souldn't change AST",
+        (file) => {
+            const filePath = __dirname + "/formatting/proper/" + file
+            const src = fs.readFileSync(filePath, "utf-8")
+            const ast = parse(src, filePath, "user")
+            //TODO: change for proper recursive removal
+            const astStr = JSONBig.stringify(ast).replace(/"id":[0-9]+,/g, "")
+
+            const formatted = formatAST(ast)
+            fs.openSync(join(__dirname, "formatting", "output", file), 'w')
+            fs.writeFileSync(join(__dirname, "formatting", "output", file), formatted, {flag: 'w'})
+            const astFormatted = parse(formatted, __dirname + "/formatting/output/" + file, "user")
+            //TODO: change for proper recursive removal
+            const astFormattedStr = JSONBig.stringify(astFormatted).replace(/"id":[0-9]+,/g, "")
+            expect(astFormattedStr).toEqual(astStr)
     })
 })
