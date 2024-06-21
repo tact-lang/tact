0175fdfaff625be48ab3bd4776f57b439b238156
Anton Trunov <anton@ton.org>
2024-06-22 00:10:14 +0400

    commit forgotten new file

---
+++ src/interpreter.ts
@@ -0,0 +1,27 @@
+import { evalConstantExpression } from "./constEval";
+import { CompilerContext } from "./context";
+import { TactConstEvalError, TactParseError } from "./errors";
+import { parseExpression } from "./grammar/grammar";
+import { Value } from "./types/types";
+
+export type EvalResult =
+    | { kind: "ok"; value: Value }
+    | { kind: "error"; message: string };
+
+export function parseAndEvalExpression(sourceCode: string): EvalResult {
+    try {
+        const ast = parseExpression(sourceCode);
+        const constEvalResult = evalConstantExpression(
+            ast,
+            new CompilerContext(),
+        );
+        return { kind: "ok", value: constEvalResult };
+    } catch (error) {
+        if (
+            error instanceof TactParseError ||
+            error instanceof TactConstEvalError
+        )
+            return { kind: "error", message: error.message };
+        throw error;
+    }
+}
