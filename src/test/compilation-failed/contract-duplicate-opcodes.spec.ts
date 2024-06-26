17efa1886aeef2a2cef42856d7479bc3abe2f436
Anton Trunov <anton@ton.org>
2024-06-26 22:56:54 +0400

    fix: detection of non-unique message opcodes (#493)

---
+++ src/test/compilation-failed/contract-duplicate-opcodes.spec.ts
@@ -0,0 +1,38 @@
+import { __DANGER_resetNodeId } from "../../grammar/ast";
+import { consoleLogger } from "../../logger";
+import { itShouldNotCompile } from "./util";
+
+describe("contract-duplicate-opcodes", () => {
+    beforeAll(() => {
+        jest.spyOn(consoleLogger, "error").mockImplementation(() => {});
+        jest.spyOn(consoleLogger, "log").mockImplementation(() => {});
+    });
+
+    beforeEach(() => {
+        __DANGER_resetNodeId();
+    });
+
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    itShouldNotCompile({
+        testName: "contract-duplicate-bounced-opcode",
+        errorMessage:
+            'Receive functions of a contract or trait cannot process messages with the same opcode: opcodes of message types "Msg2" and "Msg1" are equal',
+    });
+    itShouldNotCompile({
+        testName: "contract-duplicate-external-opcode",
+        errorMessage:
+            'Receive functions of a contract or trait cannot process messages with the same opcode: opcodes of message types "Msg2" and "Msg1" are equal',
+    });
+    itShouldNotCompile({
+        testName: "contract-duplicate-receiver-opcode",
+        errorMessage:
+            'Receive functions of a contract or trait cannot process messages with the same opcode: opcodes of message types "Msg2" and "Msg1" are equal',
+    });
+});
