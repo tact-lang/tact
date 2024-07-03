c9e383a6e3a8181baa6478d3d656c04ee39eb0fe
Daniil Sedov <42098239+Gusarich@users.noreply.github.com>
2024-07-03 09:10:13 +0300

    fix: traverse of `init` AST when filling `dependsOn` for `initOf` calls (#521)

--- src/test/e2e-emulated/initof.spec.ts
+++ src/test/e2e-emulated/initof.spec.ts
@@ -4,6 +4,8 @@ import { __DANGER_resetNodeId } from "../../grammar/ast";
 import { Self } from "./contracts/output/initof_Self";
 import { Parent } from "./contracts/output/initof_Parent";
 import { consoleLogger } from "../../logger";
+import { TestInit } from "./contracts/output/initof-2_TestInit";
+import { A } from "./contracts/output/initof-3_A";
 
 describe("initOf", () => {
     beforeAll(() => {
@@ -47,5 +49,37 @@ describe("initOf", () => {
             await contract.getTestMyAddressChild()
         ).toRawString();
         expect(addrChildInitOf).toEqual(addrChildMyAddress);
+    });
+    it("should implement initof correctly - 3", async () => {
+        // Init
+        const system = await ContractSystem.create();
+        const treasure = system.treasure("treasure");
+        const contract = system.open(await TestInit.fromInit());
+        const logger = system.log(contract.address);
+        await contract.send(
+            treasure,
+            { value: toNano("10") },
+            {
+                $$type: "Deploy",
+                queryId: 0n,
+            },
+        );
+        await system.run();
+
+        const res = logger.collect();
+
+        expect(res.includes("init@TestInit-SUCCESS")).toBe(true);
+        expect(res.includes("ERROR@TestInit")).toBe(false);
+    });
+    it("should implement initof correctly - 4", async () => {
+        // Init
+        const system = await ContractSystem.create();
+        const treasure = system.treasure("treasure");
+        const contract = system.open(await A.fromInit());
+        const tracker = system.track(contract.address);
+        await contract.send(treasure, { value: toNano("10") }, "aa");
+        await system.run();
+
+        expect(tracker.collect()).toMatchSnapshot();
     });
 });
