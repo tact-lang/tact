5f9b8fc9965b1288370c0413aa4a96ad65d566b9
Gusarich <danielsedovzzz@gmail.com>
2024-07-12 20:16:19 +0300

    add line content to debug prints

--- src/test/e2e-emulated/debug.spec.ts
+++ src/test/e2e-emulated/debug.spec.ts
@@ -35,21 +35,29 @@ describe("debug", () => {
             "src/test/e2e-emulated/contracts/debug.tact",
         );
 
-        expect(debugLogs).toStrictEqual(`File ${filePath}:10:9
+        expect(debugLogs).toStrictEqual(`File ${filePath}:10:9:
+dumpStack();
 stack(2 values) : 10000000000 () 
-File ${filePath}:11:9
+File ${filePath}:11:9:
+dump("Hello world!");
 Hello world!
-File ${filePath}:12:9
+File ${filePath}:12:9:
+dump(123);
 123
-File ${filePath}:13:9
+File ${filePath}:13:9:
+dump(true);
 true
-File ${filePath}:14:9
+File ${filePath}:14:9:
+dump(false);
 false
-File ${filePath}:15:9
+File ${filePath}:15:9:
+dump(null);
 null
-File ${filePath}:16:9
+File ${filePath}:16:9:
+dump(myAddress());
 ${contract.address.toString({ bounceable: true })}
-File ${filePath}:17:9
+File ${filePath}:17:9:
+dump(newAddress(0, 0x83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8));
 ${Address.parseRaw(
     "0:83dfd552e63729b472fcbcc8c45ebcc6691702558b68ec7527e1ba403a0f31a8",
 ).toString()}`);
