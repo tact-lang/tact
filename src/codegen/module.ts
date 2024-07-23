2a058ecfcb38bc42ae46c4ae6fb36e519a76e06c
Byakuren Hijiri <seclusion5873@proton.me>
2024-07-23 02:01:46 +0000

    feat(codegen): Support init and initChild functions

--- src/codegen/module.ts
+++ src/codegen/module.ts
@@ -1,7 +1,10 @@
 import { getAllTypes, getType } from "../types/resolveDescriptors";
+import { enabledInline, enabledMasterchain } from "../config/features";
+import { LiteralGen, Location } from ".";
 import {
     ReceiverDescription,
     TypeDescription,
+    InitDescription,
     TypeRef,
     FunctionDescription,
 } from "../types/types";
@@ -15,8 +18,9 @@ import {
     resolveFuncType,
     resolveFuncTupleType,
 } from "./type";
+import { resolveFuncPrimitive } from "./primitive";
 import { getSupportedInterfaces } from "../types/getSupportedInterfaces";
-import { funcIdOf, ops } from "./util";
+import { funcIdOf, funcInitIdOf, ops } from "./util";
 import {
     UNIT_TYPE,
     FuncAstModule,
@@ -188,16 +192,267 @@ export class ModuleGen {
     }
 
     /**
+     * Adds the init function and the init utility function to the context.
+     *
+     * TODO: Create two separate functions when refactoring
+     */
+    private writeInit(t: TypeDescription, init: InitDescription) {
+        {
+            const returnTy = resolveFuncType(this.ctx.ctx, t);
+            const funName = ops.contractInit(t.name);
+            const paramValues: FunParamValue[] = init.params.map((v) => [
+                funcIdOf(v.name),
+                resolveFuncType(this.ctx.ctx, v.type),
+            ]);
+            const attrs = [FunAttr.impure()];
+            const body: FuncAstStmt[] = [];
+
+            // Unpack parameters
+            for (const a of init.params) {
+                if (!resolveFuncPrimitive(this.ctx.ctx, a.type)) {
+                    body.push(
+                        vardef(
+                            undefined,
+                            resolveFuncTypeUnpack(
+                                this.ctx.ctx,
+                                a.type,
+                                funcIdOf(a.name),
+                            ),
+                            id(funcIdOf(a.name)),
+                        ),
+                    );
+                }
+            }
+
+            // Generate self initial tensor
+            const initValues: FuncAstExpr[] = t.fields.map((tField) =>
+                tField.default === undefined
+                    ? call("null", [])
+                    : LiteralGen.fromTact(
+                          this.ctx,
+                          tField.default!,
+                      ).writeValue(),
+            );
+            if (initValues.length > 0) {
+                // Special case for empty contracts
+                body.push(
+                    vardef(
+                        undefined,
+                        resolveFuncTypeUnpack(
+                            this.ctx.ctx,
+                            t,
+                            funcIdOf("self"),
+                        ),
+                        tensor(...initValues),
+                    ),
+                );
+            } else {
+                body.push(
+                    vardef(Type.tuple(), funcIdOf("self"), call("null", [])),
+                );
+            }
+
+            // Generate statements
+            const returns = resolveFuncTypeUnpack(
+                this.ctx.ctx,
+                t,
+                funcIdOf("self"),
+            );
+            for (const s of init.ast.statements) {
+                body.push(
+                    StatementGen.fromTact(
+                        this.ctx,
+                        s,
+                        returns,
+                    ).writeStatement(),
+                );
+            }
+
+            // Return result
+            if (
+                init.ast.statements.length === 0 ||
+                init.ast.statements[init.ast.statements.length - 1]!.kind !==
+                    "statement_return"
+            ) {
+                body.push(ret(id(returns)));
+            }
+
+            const initFun = fun(attrs, funName, paramValues, returnTy, body);
+            this.ctx.addFunction(initFun);
+        }
+
+        {
+            const returnTy = Type.tensor(Type.cell(), Type.cell());
+            const funName = ops.contractInitChild(t.name);
+            const paramValues: FunParamValue[] = [
+                ["sys", Type.cell()],
+                ...init.params.map(
+                    (v) =>
+                        [
+                            funcIdOf(v.name),
+                            resolveFuncType(this.ctx.ctx, v.type),
+                        ] as FunParamValue,
+                ),
+            ];
+            const attrs = [
+                ...(enabledInline(this.ctx.ctx) ? [FunAttr.inline()] : []),
+            ];
+            const body: FuncAstStmt[] = [];
+
+            // slice sc' = sys'.begin_parse();
+            // cell source = sc'~load_dict();
+            // cell contracts = new_dict();
+            //
+            // ;; Contract Code: ${t.name}
+            // cell mine = __tact_dict_get_code(source, ${t.uid});
+            // contracts = __tact_dict_set_code(contracts, ${t.uid}, mine);
+            body.push(
+                vardef(
+                    Type.slice(),
+                    "sc'",
+                    call("begin_parse", [], { receiver: id("sys'") }),
+                ),
+            );
+            body.push(vardef(Type.cell(), "source", call("sc'~load_dict", [])));
+            body.push(vardef(Type.cell(), "contracts", call("new_dict", [])));
+            body.push(cr());
+            body.push(comment(`Contract Code: ${t.name}`));
+            body.push(
+                vardef(
+                    Type.cell(),
+                    "mine",
+                    call("__tact_dict_get_code", [id("source"), number(t.uid)]),
+                ),
+            );
+            body.push(
+                expr(
+                    assign(
+                        id("contracts"),
+                        call("__tact_dict_set_code", [
+                            id("contracts"),
+                            number(t.uid),
+                            id("mine"),
+                        ]),
+                    ),
+                ),
+            );
+
+            // Copy contracts code
+            for (const c of t.dependsOn) {
+                // ;; Contract Code: ${c.name}
+                // cell code_${c.uid} = __tact_dict_get_code(source, ${c.uid});
+                // contracts = __tact_dict_set_code(contracts, ${c.uid}, code_${c.uid});
+                body.push(cr());
+                body.push(comment(`Contract Code: ${c.name}`));
+                body.push(
+                    vardef(
+                        Type.cell(),
+                        `code_${c.uid}`,
+                        call("__tact_dict_get_code", [
+                            id("source"),
+                            number(c.uid),
+                        ]),
+                    ),
+                );
+                body.push(
+                    expr(
+                        assign(
+                            id("contracts"),
+                            call("__tact_dict_set_code", [
+                                id("contracts"),
+                                number(c.uid),
+                                id(`code_${c.uid}`),
+                            ]),
+                        ),
+                    ),
+                );
+            }
+
+            // Build cell
+            body.push(cr());
+            body.push(comment("Build cell"));
+            body.push(vardef(Type.builder(), "b", call("begin_cell", [])));
+            // b = b.store_ref(begin_cell().store_dict(contracts).end_cell());
+            body.push(
+                expr(
+                    assign(
+                        id("b"),
+                        call(
+                            "store_ref",
+                            [
+                                call("end_cell", [], {
+                                    receiver: call(
+                                        "store_dict",
+                                        [id("contracts")],
+                                        { receiver: call("begin_cell", []) },
+                                    ),
+                                }),
+                            ],
+                            { receiver: id("b") },
+                        ),
+                    ),
+                ),
+            );
+            // b = b.store_int(false, 1);
+            body.push(
+                expr(
+                    assign(
+                        id("b"),
+                        call("store_int", [bool(false), number(1)], {
+                            receiver: id("b"),
+                        }),
+                    ),
+                ),
+            );
+            const args =
+                t.init!.params.length > 0
+                    ? [
+                          call(
+                              "b",
+                              t.init!.params.map((a) => id(funcIdOf(a.name))),
+                          ),
+                      ]
+                    : [id("b"), call("null", [])];
+            body.push(
+                expr(
+                    assign(
+                        id("b"),
+                        call(`${ops.writer(funcInitIdOf(t.name))}`, args),
+                    ),
+                ),
+            );
+            body.push(
+                ret(
+                    tensor(
+                        id("mine"),
+                        call("end_cell", [], { receiver: id("b") }),
+                    ),
+                ),
+            );
+
+            this.ctx.addFunction(
+                fun(attrs, funName, paramValues, returnTy, body),
+                { context: Location.type("type:" + t.name + "$init") },
+            );
+        }
+    }
+
+    /**
      * Adds functions defined within the Tact contract to the generated Func module.
      * TODO: Why do we need function from *all* the contracts?
      */
     private addContractFunctions(m: FuncAstModule, c: TypeDescription): void {
         m.entries.push(comment("", `Contract ${c.name} functions`, ""));
 
+        if (c.init) {
+            this.writeInit(c, c.init);
+        }
+
         for (const tactFun of c.functions.values()) {
             const funcFun = FunctionGen.fromTact(this.ctx).writeFunction(
                 tactFun,
             );
+            // TODO: Should we really put them here?
             m.entries.push(funcFun);
         }
     }
