f6bec4bc68a9e9f87dd4e674a6387171a002003c
Daniil Sedov <danielsedovzzz@gmail.com>
2024-07-24 14:35:01 +0300

    fix: allow structs with more than 15 fields (#590)

--- src/test/e2e-emulated/structs.spec.ts
+++ src/test/e2e-emulated/structs.spec.ts
@@ -239,5 +239,14 @@ describe("structs", () => {
         await contract.send(treasure, { value: toNano("10") }, "example");
         await system.run();
         expect(tracker.collect()).toMatchSnapshot();
+
+        expect(await contract.getLongStruct15Test()).toMatchSnapshot();
+        expect(await contract.getLongStruct16Test()).toMatchSnapshot();
+        expect(await contract.getLongStruct32Test()).toMatchSnapshot();
+        expect(await contract.getLongNestedStructTest()).toMatchSnapshot();
+        expect(
+            await contract.getLongNestedStructWithOptsTest(),
+        ).toMatchSnapshot();
+        expect(await contract.getLongContractTest()).toEqual(210n);
     });
 });
