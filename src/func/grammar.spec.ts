8d32ff8865c9670b363c90a887630b8efa0f8982
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-07-27 01:21:28 +0200

    feat(func-parser): mostly finished the expressions and fixed types

--- src/func/grammar.spec.ts
+++ src/func/grammar.spec.ts
@@ -1,11 +1,13 @@
 import { match } from "./grammar";
 import { loadCases } from "../utils/loadCases";
 
-describe("FunC parser", () => {
+describe("FunC grammar and parser", () => {
     beforeEach(() => {});
 
     // Checking that FunC files match the grammar
-    for (const r of loadCases(__dirname + "/test/")) {
+    for (const r of loadCases(__dirname + "/grammar-test/", "fc")) {
+        // if (r.name !== "include_stdlib") continue;
+
         it(r.name + " should match the grammar", () => {
             expect(match(r.code).ok).toStrictEqual(true);
         });
