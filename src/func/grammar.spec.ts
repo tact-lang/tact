8bc2486e496a6d00cca452b2c78ed28ed204a0c2
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-07-29 00:11:03 +0200

    feat: tested top-level things, added more tests for identifiers
    Fully tested:
    * Asm Functions
    * Globals
    * Pragmas
    * Constants
    
    Partially tested: Function definitions and declarations
    Next up: Statements
    
    Broght over identifier tests from the funcId fixing PRs. Almost re-wrote
    the universally correct identifier recognition, stuck with underscores
    for a bit. Will move past that and finilize all other things, up to (and
    perhaps including) expressions + semantics. In the background I'll try
    to polish identifiers some more :)

--- src/func/grammar.spec.ts
+++ src/func/grammar.spec.ts
@@ -4,12 +4,27 @@ import { loadCases } from "../utils/loadCases";
 describe("FunC grammar and parser", () => {
     beforeEach(() => {});
 
+    const target = "asm-functions";
+    // const target = "identifiers";
+
     // Checking that FunC files match the grammar
     for (const r of loadCases(__dirname + "/grammar-test/", "fc")) {
-        // if (r.name !== "include_stdlib") continue;
-
+        if (r.name !== target) continue;
         it(r.name + " should match the grammar", () => {
-            expect(match(r.code).ok).toStrictEqual(true);
+            const res = match(r.code);
+
+            if (res.ok === false) {
+                console.log(res.message, res.interval.getLineAndColumn());
+            }
+
+            expect(res.ok).toStrictEqual(true);
+        });
+    }
+
+    // Checking that certain FunC files DON'T match the grammar
+    for (const r of loadCases(__dirname + "/grammar-test-failed/", "fc")) {
+        it(r.name + " should not match the grammar", () => {
+            expect(match(r.code).ok).toStrictEqual(false);
         });
     }
 
