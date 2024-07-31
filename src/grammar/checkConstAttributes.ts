2524aae05b558deaab3ce01086aefc21d65373c8
Anton Trunov <anton@ton.org>
2024-07-31 21:07:06 +0400

    fix: module-level constants cannot have any attributes (#644)
    - add negative tests for duplicate const attributes and fix
      the error message (`overrides` -> `override`)
    - introduce `TactSyntaxError`

--- src/grammar/checkConstAttributes.ts
+++ src/grammar/checkConstAttributes.ts
@@ -1,5 +1,5 @@
 import { AstConstantAttribute, SrcInfo } from "./ast";
-import { throwCompilationError } from "../errors";
+import { throwSyntaxError } from "../errors";
 
 export function checkConstAttributes(
     isAbstract: boolean,
@@ -9,23 +9,20 @@ export function checkConstAttributes(
     const k: Set<string> = new Set();
     for (const a of attributes) {
         if (k.has(a.type)) {
-            throwCompilationError(
-                `Duplicate constant attribute "${a.type}"`,
-                a.loc,
-            );
+            throwSyntaxError(`Duplicate constant attribute "${a.type}"`, a.loc);
         }
         k.add(a.type);
     }
     if (isAbstract) {
         if (!k.has("abstract")) {
-            throwCompilationError(
+            throwSyntaxError(
                 `Abstract constant doesn't have abstract modifier`,
                 loc,
             );
         }
     } else {
         if (k.has("abstract")) {
-            throwCompilationError(
+            throwSyntaxError(
                 `Non-abstract constant has abstract modifier`,
                 loc,
             );
