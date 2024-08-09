d2e052ed075d52faf3870f1981a64ca9afd157ff
Georgiy Komarov <jubnzv@gmail.com>
2024-08-09 02:10:21 +0000

    feat: Add errors to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 export { enableFeatures, build } from "./pipeline/build";
+export {
+    TactError,
+    TactParseError,
+    TactSyntaxError,
+    TactCompilationError,
+    TactInternalCompilerError,
+    TactConstEvalError,
+    TactErrorCollection,
+} from "./errors";
