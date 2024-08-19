778e247077105d677ed1d7382ea4c7b928e20c5d
Gusarich <danielsedovzzz@gmail.com>
2024-08-19 12:14:17 +0300

    wip

--- src/test/e2e-emulated/map-comparison.spec.ts
+++ src/test/e2e-emulated/map-comparison.spec.ts
@@ -1,4 +1,4 @@
-import { Dictionary, toNano } from "@ton/core";
+import { Address, beginCell, Cell, Dictionary, toNano } from "@ton/core";
 import { ContractSystem } from "@tact-lang/emulator";
 import { __DANGER_resetNodeId } from "../../grammar/ast";
 import { MapComparisonTestContract } from "./contracts/output/map-comparison_MapComparisonTestContract";
@@ -14,10 +14,13 @@ describe("map-comparison", () => {
         const contract = system.open(
             await MapComparisonTestContract.fromInit(),
         );
-        const tracker = system.track(contract.address);
+        await contract.send(treasure, { value: toNano("10") }, null);
+        await system.run();
 
         // Test
+
         {
+            // Int Int - Equal
             const m1: Dictionary<bigint, bigint> = Dictionary.empty(
                 Dictionary.Keys.BigInt(256),
                 Dictionary.Values.BigInt(256),
@@ -30,17 +33,132 @@ describe("map-comparison", () => {
             );
             m2.set(1n, 2n);
             m2.set(3n, 4n);
-            await contract.send(
-                treasure,
-                { value: toNano("10") },
-                {
-                    $$type: "CompareIntInt",
-                    m1,
-                    m2,
-                },
+            expect(await contract.getCompareIntInt(m1, m2)).toBe(true);
+        }
+
+        {
+            // Int Int - Not Equal
+            const m1: Dictionary<bigint, bigint> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.BigInt(256),
             );
-            await system.run();
-            expect(tracker.collect()).toMatchSnapshot();
+            m1.set(1n, 2n);
+            m1.set(3n, 4n);
+            const m2: Dictionary<bigint, bigint> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.BigInt(256),
+            );
+            m2.set(1n, 2n);
+            m2.set(3n, 5n);
+            expect(await contract.getCompareIntInt(m1, m2)).toBe(false);
+        }
+
+        {
+            // Int Cell - Equal
+            const m1: Dictionary<bigint, Cell> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Cell(),
+            );
+            m1.set(1n, beginCell().storeUint(123, 64).endCell());
+            m1.set(3n, beginCell().storeUint(456, 64).endCell());
+            const m2: Dictionary<bigint, Cell> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Cell(),
+            );
+            m2.set(1n, beginCell().storeUint(123, 64).endCell());
+            m2.set(3n, beginCell().storeUint(456, 64).endCell());
+            expect(await contract.getCompareIntCell(m1, m2)).toBe(true);
+        }
+
+        {
+            // Int Cell - Not Equal
+            const m1: Dictionary<bigint, Cell> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Cell(),
+            );
+            m1.set(1n, beginCell().storeUint(123, 64).endCell());
+            m1.set(3n, beginCell().storeUint(456, 64).endCell());
+            const m2: Dictionary<bigint, Cell> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Cell(),
+            );
+            m2.set(1n, beginCell().storeUint(123, 64).endCell());
+            m2.set(3n, beginCell().storeUint(457, 64).endCell());
+            expect(await contract.getCompareIntCell(m1, m2)).toBe(false);
+        }
+
+        {
+            // Int Address - Equal
+            const m1: Dictionary<bigint, Address> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Address(),
+            );
+            m1.set(
+                1n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000002",
+                ),
+            );
+            m1.set(
+                3n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000004",
+                ),
+            );
+            const m2: Dictionary<bigint, Address> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Address(),
+            );
+            m2.set(
+                1n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000002",
+                ),
+            );
+            m2.set(
+                3n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000004",
+                ),
+            );
+            expect(await contract.getCompareIntAddress(m1, m2)).toBe(true);
+        }
+
+        {
+            // Int Address - Not Equal
+            const m1: Dictionary<bigint, Address> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Address(),
+            );
+            m1.set(
+                1n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000002",
+                ),
+            );
+            m1.set(
+                3n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000004",
+                ),
+            );
+            const m2: Dictionary<bigint, Address> = Dictionary.empty(
+                Dictionary.Keys.BigInt(256),
+                Dictionary.Values.Address(),
+            );
+            m2.set(
+                1n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000002",
+                ),
+            );
+            m2.set(
+                3n,
+                Address.parseRaw(
+                    "0:0000000000000000000000000000000000000000000000000000000000000005",
+                ),
+            );
+            expect(await contract.getCompareIntAddress(m1, m2)).toBe(false);
         }
     });
 });
