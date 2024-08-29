ed2c9820c8b29b4bf30f4d1f05911e199d06c97a
Georgiy Komarov <jubnzv@gmail.com>
2024-08-29 02:57:19 +0000

    feat(iterators): Support new grammar

--- src/func/iterators.ts
+++ src/func/iterators.ts
@@ -1,4 +1,4 @@
-import { FuncAstNode, FuncAstExpr } from "./syntax";
+import { FuncAstNode, FuncAstExpression } from "./grammar";
 
 import JSONbig from "json-bigint";
 
@@ -10,142 +10,175 @@ import JSONbig from "json-bigint";
  */
 export function forEachExpression(
     node: FuncAstNode,
-    callback: (expr: FuncAstExpr) => void,
+    callback: (expr: FuncAstExpression) => void,
 ): void {
-    if ("kind" in node) {
-        switch (node.kind) {
-            // Expressions
-            case "id_expr":
-            case "number_expr":
-            case "hex_number_expr":
-            case "bool_expr":
-            case "string_expr":
-            case "nil_expr":
-            case "unit_expr":
-            case "primitive_type_expr":
-                callback(node);
-                break;
-            case "call_expr":
-                if (node.receiver) forEachExpression(node.receiver, callback);
-                forEachExpression(node.fun, callback);
-                node.args.forEach((arg) => forEachExpression(arg, callback));
-                callback(node);
-                break;
-            case "assign_expr":
-            case "augmented_assign_expr":
-                forEachExpression(node.lhs, callback);
-                forEachExpression(node.rhs, callback);
-                callback(node);
-                break;
-            case "ternary_expr":
-                forEachExpression(node.cond, callback);
-                forEachExpression(node.trueExpr, callback);
-                forEachExpression(node.falseExpr, callback);
-                callback(node);
-                break;
-            case "binary_expr":
-                forEachExpression(node.lhs, callback);
-                forEachExpression(node.rhs, callback);
-                callback(node);
-                break;
-            case "unary_expr":
-                forEachExpression(node.value, callback);
-                callback(node);
-                break;
-            case "apply_expr":
-                forEachExpression(node.lhs, callback);
-                forEachExpression(node.rhs, callback);
-                callback(node);
-                break;
-            case "tuple_expr":
-            case "tensor_expr":
-                node.values.forEach((value) =>
-                    forEachExpression(value, callback),
+    switch (node.kind) {
+        case "expression_assign":
+            callback(node);
+            forEachExpression(node.left, callback);
+            forEachExpression(node.right, callback);
+            break;
+        case "expression_conditional":
+            callback(node);
+            forEachExpression(node.condition, callback);
+            forEachExpression(node.consequence, callback);
+            forEachExpression(node.alternative, callback);
+            break;
+        case "expression_compare":
+            callback(node);
+            forEachExpression(node.left, callback);
+            forEachExpression(node.right, callback);
+            break;
+        case "expression_bitwise_shift":
+        case "expression_add_bitwise":
+        case "expression_mul_bitwise":
+            callback(node);
+            forEachExpression(node.left, callback);
+            node.ops.forEach((part) => forEachExpression(part.expr, callback));
+            break;
+        case "expression_unary":
+            callback(node);
+            forEachExpression(node.operand, callback);
+            break;
+        case "expression_method":
+            callback(node);
+            forEachExpression(node.object, callback);
+            node.calls.forEach((call) =>
+                forEachExpression(call.argument, callback),
+            );
+            break;
+        case "expression_var_decl":
+            callback(node);
+            if (
+                node.names.kind === "expression_tensor_var_decl" ||
+                node.names.kind === "expression_tuple_var_decl"
+            ) {
+                node.names.names.forEach((name) =>
+                    forEachExpression(name, callback),
                 );
-                callback(node);
-                break;
-            case "hole_expr":
-                forEachExpression(node.init, callback);
-                callback(node);
-                break;
-
-            // Statements
-            case "var_def_stmt":
-                if (node.init) forEachExpression(node.init, callback);
-                break;
-            case "return_stmt":
-                if (node.value) forEachExpression(node.value, callback);
-                break;
-            case "block_stmt":
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                break;
-            case "repeat_stmt":
-                forEachExpression(node.condition, callback);
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                break;
-            case "condition_stmt":
-                if (node.condition) forEachExpression(node.condition, callback);
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                if (node.else) forEachExpression(node.else, callback);
-                break;
-            case "do_until_stmt":
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                forEachExpression(node.condition, callback);
-                break;
-            case "while_stmt":
-                forEachExpression(node.condition, callback);
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                break;
-            case "expr_stmt":
-                forEachExpression(node.expr, callback);
-                break;
-            case "try_catch_stmt":
-                node.tryBlock.forEach((stmt) =>
+            } else {
+                forEachExpression(node.names, callback);
+            }
+            break;
+        case "expression_fun_call":
+            callback(node);
+            forEachExpression(node.object, callback);
+            node.arguments.forEach((arg) => forEachExpression(arg, callback));
+            break;
+        case "expression_tensor":
+        case "expression_tuple":
+            callback(node);
+            node.expressions.forEach((expr) =>
+                forEachExpression(expr, callback),
+            );
+            break;
+        case "module":
+            node.items.forEach((item) => forEachExpression(item, callback));
+            break;
+        case "pragma_literal":
+        case "pragma_version_range":
+        case "pragma_version_string":
+        case "include":
+            break;
+        case "global_variables_declaration":
+            // Do nothing; global variables don't contain initializers
+            break;
+        case "constants_definition":
+            node.constants.forEach((c) => forEachExpression(c.value, callback));
+            break;
+        case "asm_function_definition":
+            break;
+        case "function_declaration":
+        case "function_definition":
+            forEachExpression(node.name, callback);
+            node.attributes.forEach((attr) => {
+                if (attr.kind === "method_id" && attr.value) {
+                    forEachExpression(attr.value, callback);
+                }
+            });
+            if (node.kind === "function_definition") {
+                node.statements.forEach((stmt) =>
                     forEachExpression(stmt, callback),
                 );
-                node.catchBlock.forEach((stmt) =>
+            }
+            break;
+        case "unit":
+            break;
+        case "statement_return":
+            if (node.expression) forEachExpression(node.expression, callback);
+            break;
+        case "statement_block":
+            node.statements.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            break;
+        case "statement_empty":
+            break;
+        case "statement_condition_if":
+            forEachExpression(node.condition, callback);
+            node.consequences.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            if (node.alternatives) {
+                node.alternatives.forEach((stmt) =>
                     forEachExpression(stmt, callback),
                 );
-                break;
-
-            // Other and top-level elements
-            case "constant":
-                forEachExpression(node.init, callback);
-                break;
-            case "function_definition":
-                node.body.forEach((stmt) => forEachExpression(stmt, callback));
-                break;
-            case "asm_function_definition":
-                // Do nothing; we don't iterate raw asm
-                break;
-            case "comment":
-            case "cr":
-            case "include":
-            case "pragma":
-            case "global_variable":
-            case "function_declaration":
-                // These nodes don't contain expressions, so we don't need to do anything
-                break;
-            case "module":
-                node.entries.forEach((entry) =>
-                    forEachExpression(entry, callback),
+            }
+            break;
+        case "statement_condition_elseif":
+            forEachExpression(node.conditionIf, callback);
+            node.consequencesIf.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            forEachExpression(node.conditionElseif, callback);
+            node.consequencesElseif.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            if (node.alternativesElseif) {
+                node.alternativesElseif.forEach((stmt) =>
+                    forEachExpression(stmt, callback),
                 );
-                break;
+            }
+            break;
+        case "statement_repeat":
+            forEachExpression(node.iterations, callback);
+            node.statements.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            break;
+        case "statement_until":
+        case "statement_while":
+            forEachExpression(node.condition, callback);
+            node.statements.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            break;
+        case "statement_try_catch":
+            node.statementsTry.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            node.statementsCatch.forEach((stmt) =>
+                forEachExpression(stmt, callback),
+            );
+            break;
+        case "statement_expression":
+            forEachExpression(node.expression, callback);
+            break;
+        // No nested nodes for these cases
+        case "method_id":
+        case "quoted_id":
+        case "operator_id":
+        case "plain_id":
+        case "unused_id":
+        case "integer_literal":
+        case "string_singleline":
+        case "string_multiline":
+        case "cr":
+        case "comment_singleline":
+        case "comment_multiline":
+            break;
 
-            // FuncType cases
-            case "int":
-            case "cell":
-            case "slice":
-            case "builder":
-            case "cont":
-            case "tuple":
-            case "tensor":
-            case "hole":
-            case "type":
-                break;
-
-            default:
-                throw new Error(`Unsupported node: ${JSONbig.stringify(node)}`);
-        }
+        default:
+            throw new Error(`Unsupported node: ${JSON.stringify(node)}`);
     }
 }
