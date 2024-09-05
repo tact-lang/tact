f201331b54f618ccd31981ba0ae0094aea82a5ba
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-09-05 12:29:21 +0200

    feat: make a new folder for exit code tests and apply suggestions

--- src/test/exit-codes/repeat-range.spec.ts
+++ src/test/exit-codes/repeat-range.spec.ts
@@ -9,11 +9,9 @@ describe("repeat range", () => {
     let contract: SandboxContract<RepeatRange>;
 
     beforeEach(async () => {
-        const LARGE_SUM = 10_000_000_000n;
-
         blockchain = await Blockchain.create();
+        blockchain.verbosity.print = false;
         treasure = await blockchain.treasury("treasure", {
-            balance: LARGE_SUM,
             resetBalanceIfZero: true,
         });
 
@@ -31,37 +29,29 @@ describe("repeat range", () => {
             success: true,
             deploy: true,
         });
-
-        const topUpResult = await treasure.send({
-            to: contract.address,
-            value: LARGE_SUM,
-        });
-
-        expect(topUpResult.transactions).toHaveTransaction({
-            from: treasure.address,
-            to: contract.address,
-            success: true,
-        });
     });
 
     it("should test repeat range boundaries", async () => {
         // ignored range
-        expect(await contract.getTestIgnoredRange()).toEqual(0n);
+        expect(await contract.getTestIgnoredRange()).toEqual(true);
 
         // invalid range
-        expect(await contract.getTestInvalidRange()).toEqual(1n);
+        expect(await contract.getTestInvalidRange()).toEqual(true);
 
-        // effective range
+        // min effective range
+        expect(await contract.getTestMinEffectiveRange()).toEqual(true);
+
+        // max effective range
         const sendResult = await contract.send(
             treasure.getSender(),
             { value: toNano("10") },
-            "effectiveRange",
+            "testMaxEffectiveRange",
         );
         expect(sendResult.transactions).toHaveTransaction({
             from: treasure.address,
             to: contract.address,
-            success: true,
+            success: false,
+            exitCode: -14,
         });
-        expect(await contract.getEffectiveRangeCounter()).toEqual(2n ** 31n);
     });
 });
