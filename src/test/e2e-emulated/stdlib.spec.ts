2898ef6cb467396168650aea43b9c8c6dcd0e69e
Gusarich <danielsedovzzz@gmail.com>
2024-09-05 10:37:06 +0300

    feat: add `parseStdAddress` and `parseVarAddress`

--- src/test/e2e-emulated/stdlib.spec.ts
+++ src/test/e2e-emulated/stdlib.spec.ts
@@ -1,4 +1,4 @@
-import { beginCell, toNano } from "@ton/core";
+import { Address, beginCell, toNano } from "@ton/core";
 import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { StdlibTest } from "./contracts/output/stdlib_StdlibTest";
 import "@ton/test-utils";
@@ -74,5 +74,36 @@ describe("stdlib", () => {
         expect(
             (await contract.getStoreMaybeRef(beginCell(), null)).endCell(),
         ).toEqualCell(beginCell().storeMaybeRef(null).endCell());
+
+        const addrStd = await contract.getParseStdAddress(
+            beginCell()
+                .storeAddress(
+                    Address.parse(
+                        "0:4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873",
+                    ),
+                )
+                .endCell()
+                .asSlice(),
+        );
+        expect(addrStd.workchain).toBe(0n);
+        expect(addrStd.address).toBe(
+            BigInt(
+                "0x4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873",
+            ),
+        );
+
+        const addrVar = await contract.getParseVarAddress(
+            beginCell()
+                .storeUint(6, 3)
+                .storeUint(123, 9)
+                .storeUint(234, 32)
+                .storeUint(345, 123)
+                .endCell()
+                .asSlice(),
+        );
+        expect(addrVar.workchain).toBe(234n);
+        expect(addrVar.address.asCell()).toEqualCell(
+            beginCell().storeUint(345, 123).endCell(),
+        );
     });
 });
