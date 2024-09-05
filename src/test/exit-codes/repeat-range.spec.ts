926edb450e5b97503dcd1a7480d7c5a446f553ea
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-09-05 15:35:45 +0200

    test: check `repeat`-loop ranges (#766)

---
+++ src/test/exit-codes/repeat-range.spec.ts
@@ -0,0 +1,57 @@
+import { toNano } from "@ton/core";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
+import { RepeatRange } from "./contracts/output/repeat-range_RepeatRange";
+import "@ton/test-utils";
+
+describe("repeat range", () => {
+    let blockchain: Blockchain;
+    let treasure: SandboxContract<TreasuryContract>;
+    let contract: SandboxContract<RepeatRange>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        blockchain.verbosity.print = false;
+        treasure = await blockchain.treasury("treasure", {
+            resetBalanceIfZero: true,
+        });
+
+        contract = blockchain.openContract(await RepeatRange.fromInit());
+
+        const deployResult = await contract.send(
+            treasure.getSender(),
+            { value: toNano("10") },
+            null,
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: treasure.address,
+            to: contract.address,
+            success: true,
+            deploy: true,
+        });
+    });
+
+    it("should test repeat range boundaries", async () => {
+        // ignored range
+        expect(await contract.getTestIgnoredRange()).toEqual(true);
+
+        // invalid range
+        expect(await contract.getTestInvalidRange()).toEqual(true);
+
+        // min effective range
+        expect(await contract.getTestMinEffectiveRange()).toEqual(true);
+
+        // max effective range
+        const sendResult = await contract.send(
+            treasure.getSender(),
+            { value: toNano("10") },
+            "testMaxEffectiveRange",
+        );
+        expect(sendResult.transactions).toHaveTransaction({
+            from: treasure.address,
+            to: contract.address,
+            success: false,
+            exitCode: -14,
+        });
+    });
+});
