778882dfeaf08ead03ec807369a9f7078b42f6a5
jeshecdom <jeshecdom@hotmail.com>
2024-09-10 18:05:39 +0200

    Test cases for semantics of structs.

---
+++ src/test/e2e-emulated/semantics.spec.ts
@@ -0,0 +1,41 @@
+import { toNano } from "@ton/core";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
+import { SemanticsTester } from "./contracts/output/semantics_SemanticsTester";
+import "@ton/test-utils";
+
+describe("semantics", () => {
+    let blockchain: Blockchain;
+    let treasure: SandboxContract<TreasuryContract>;
+    let contract: SandboxContract<SemanticsTester>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        blockchain.verbosity.print = false;
+        treasure = await blockchain.treasury("treasure");
+
+        contract = blockchain.openContract(await SemanticsTester.fromInit());
+
+        const deployResult = await contract.send(
+            treasure.getSender(),
+            { value: toNano("10") },
+            null,
+        );
+        expect(deployResult.transactions).toHaveTransaction({
+            from: treasure.address,
+            to: contract.address,
+            success: true,
+            deploy: true,
+        });
+    });
+
+    it("should implement semantics correctly", async () => {
+        // Check methods
+        expect(await contract.getStructAssign1()).toEqual(true);
+        expect(await contract.getStructAssign2()).toEqual(true);
+        expect(await contract.getStructInitCheck()).toEqual(true);
+        expect(await contract.getParamStruct1()).toEqual(true);
+        expect(await contract.getParamStruct2()).toEqual(true);
+        expect(await contract.getMutateParamStruct1()).toEqual(true);
+        expect(await contract.getMutateParamStruct2()).toEqual(true);
+    });
+});
