a6f319965197dd7d5d0a15045b55878fc6787d54
Anton Trunov <anton@ton.org>
2024-09-15 10:40:47 +0400

    fix: contract method calls in `return` statements

---
+++ src/test/e2e-emulated/contract-methods.spec.ts
@@ -0,0 +1,32 @@
+import { toNano } from "@ton/core";
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
+import { Test } from "./contracts/output/contract-methods_Test";
+import "@ton/test-utils";
+
+describe("contract-methods", () => {
+    let blockchain: Blockchain;
+    let treasure: SandboxContract<TreasuryContract>;
+    let contract: SandboxContract<Test>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        blockchain.verbosity.print = false;
+        treasure = await blockchain.treasury("treasure");
+
+        contract = blockchain.openContract(await Test.fromInit());
+
+        const deployResult = await contract.send(
+            treasure.getSender(),
+            { value: toNano("10") },
+            null,
+        );
+        expect(deployResult.transactions).toHaveTransaction({
+            from: treasure.address,
+            to: contract.address,
+            success: true,
+            deploy: true,
+        });
+    });
+
+    it("should implement contract methods correctly", async () => {});
+});
