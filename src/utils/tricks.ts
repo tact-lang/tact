21808e439fdbc124f850d3bc87b67a6686467d5d
verytactical <186486509+verytactical@users.noreply.github.com>
2024-11-14 22:07:10 +0400

    refactor/fix: pretty-printer (#1018)
    * refactor: introduce DSL to handle indents, etc.
    * fix: precedence levels for expressions

--- src/utils/tricks.ts
+++ src/utils/tricks.ts
@@ -88,3 +88,49 @@ export const match = <const I extends any[]>(
         throw new Error("Not exhaustive");
     }) as MV<Flat<I>, never>;
 };
+
+import { throwInternalCompilerError } from "../errors";
+
+/**
+ * Convert union to intersection. See https://stackoverflow.com/q/50374908
+ */
+type Intersect<T> = (T extends unknown ? (x: T) => 0 : never) extends (
+    x: infer R,
+) => 0
+    ? R
+    : never;
+
+/**
+ * Makes types more readable
+ * Example: Unwrap<{ a: 1 } & { b: 2 }> = { a: 1, b: 2 }
+ */
+type Unwrap<T> = T extends infer R ? { [K in keyof R]: R[K] } : never;
+
+type Inputs<I> = I extends { kind: infer K }
+    ? K extends string
+        ? Record<K, (input: I) => unknown>
+        : never
+    : never;
+type Outputs<O> = { [K in keyof O]: (input: never) => O[K] };
+type Handlers<I, O> = Unwrap<Intersect<Inputs<I>>> & Outputs<O>;
+
+/**
+ * Make visitor for disjoint union (tagged union, discriminated union)
+ */
+export const makeVisitor =
+    <I>() =>
+    <O>(handlers: Handlers<I, O>) =>
+    (input: Extract<I, { kind: string }>): O[keyof O] => {
+        const handler = (handlers as Record<string, (input: I) => O[keyof O]>)[
+            input.kind
+        ];
+
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (handler) {
+            return handler(input);
+        } else {
+            throwInternalCompilerError(
+                `Reached impossible case: ${input.kind}`,
+            );
+        }
+    };
