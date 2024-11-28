4d05722e68f36fc40b7b331394f88136010d8cdb
Gusarich <danielsedovzzz@gmail.com>
2024-11-28 08:20:58 +0300

    feat: cover new functions with tests

--- src/test/e2e-emulated/stdlib.spec.ts
+++ src/test/e2e-emulated/stdlib.spec.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, toNano } from "@ton/core";
+import { Address, beginCell, Cell, toNano } from "@ton/core";
 import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { StdlibTest } from "./contracts/output/stdlib_StdlibTest";
 import "@ton/test-utils";
@@ -56,7 +56,7 @@ describe("stdlib", () => {
                 .toString(),
         ).toBe(beginCell().storeBit(true).endCell().toString());
 
-        expect(await contract.getTvm_2023_07Upgrade()).toEqual(1355n);
+        expect(await contract.getTvm_2023_07Upgrade()).toEqual(1455n);
         expect(await contract.getTvm_2024_04Upgrade()).toEqual(82009144n);
 
         expect(
@@ -106,5 +106,63 @@ describe("stdlib", () => {
         expect(addrVar.address.asCell()).toEqualCell(
             beginCell().storeUint(345, 123).endCell(),
         );
+
+        expect(await contract.getBuilderDepth(beginCell())).toBe(0n);
+        expect(
+            await contract.getBuilderDepth(beginCell().storeRef(Cell.EMPTY)),
+        ).toBe(1n);
+
+        expect(await contract.getSkipLastBits(slice, 1n)).toEqualSlice(
+            beginCell()
+                .storeBit(1)
+                .storeRef(beginCell().storeBit(1).endCell())
+                .endCell()
+                .asSlice(),
+        );
+
+        expect(await contract.getFirstBits(slice, 1n)).toEqualSlice(
+            beginCell().storeBit(1).endCell().asSlice(),
+        );
+
+        expect(await contract.getLastBits(slice, 1n)).toEqualSlice(
+            beginCell().storeBit(1).endCell().asSlice(),
+        );
+
+        expect(await contract.getSliceDepth(slice)).toBe(1n);
+
+        expect(await contract.getAddressNone()).toEqual(null);
+
+        expect(
+            await contract.getComputeDataSizeCell(slice.asCell(), 1000n),
+        ).toMatchObject({
+            $$type: "DataSize",
+            cells: 2n,
+            bits: 3n,
+            refs: 1n,
+        });
+
+        expect(
+            await contract.getComputeDataSizeSlice(slice, 1000n),
+        ).toMatchObject({
+            $$type: "DataSize",
+            cells: 1n, // -1 for slice
+            bits: 3n,
+            refs: 1n,
+        });
+
+        expect(await contract.getCellDepth(slice.asCell())).toBe(1n);
+
+        expect(await contract.getCurLt()).toBe(0n);
+
+        expect(await contract.getBlockLt()).toBe(0n);
+
+        expect(await contract.getSetGasLimit(5000n)).toBe(3997n); // 5000 just to make sure it's enough, 3997 is how much it actually costs
+        await expect(contract.getSetGasLimit(3996n)).rejects.toThrow("-14"); // 3996 gas is not enough for sure
+
+        expect(await contract.getGetSeed()).toBe(0n);
+
+        expect(await contract.getSetSeed(123n)).toBe(123n);
+
+        expect(await contract.getMyCode()).toEqualCell(contract.init!.code);
     });
 });
