12c32b5e5ec31075d458a787291311cfe3362394
Anton Trunov <anton@tonstudio.io>
2025-01-02 22:40:27 +0400

    feat: generate contracts and test specs from templates
    for each key-value type combination

---
+++ src/test/e2e-emulated/map-tests/generate.ts
@@ -0,0 +1,75 @@
+import { run } from "../../../node";
+import { MapType, keyTypes, valTypes } from "./map-properties-key-value-types";
+import { mkdir, writeFile } from "fs/promises";
+import { readFileSync } from "fs";
+import path from "path";
+
+const pwd = (fileName: string): string => path.join(__dirname, fileName);
+
+const testDirectory = (testName: string): string =>
+    pwd(`./build/instance_${testName}`);
+
+const testContractFileName = "test.tact";
+const specFileName = "map-properties.spec.ts";
+
+const instantiateContractTemplateAndSpec = async (
+    templateTact: string,
+    templateSpec: string,
+    testName: string,
+    key: MapType,
+    val: MapType,
+): Promise<string> => {
+    const tactSourceCode = templateTact
+        .replaceAll("KEY_TYPE", key.type)
+        .replaceAll("VAL_TYPE", val.type);
+    const testDir = testDirectory(testName);
+    await mkdir(testDir, { recursive: true });
+    const tactFilePath = path.join(testDir, testContractFileName);
+    await writeFile(tactFilePath, tactSourceCode);
+    const specSourceCode = templateSpec
+        .replaceAll("KEY_1", key.val1)
+        .replaceAll("KEY_2", key.val2)
+        .replaceAll("VAL_1", val.val1)
+        .replaceAll("VAL_2", val.val2);
+    const specFilePath = path.join(testDir, specFileName);
+    await writeFile(specFilePath, specSourceCode);
+    return tactFilePath;
+};
+
+const templateTactSourceCode: string = readFileSync(
+    pwd("map-properties.tact.template"),
+).toString();
+
+const templateSpecSourceCode: string = readFileSync(
+    pwd("map-properties.spec.ts.template"),
+).toString();
+
+const compileContracts = async () => {
+    for (const key of keyTypes) {
+        for (const val of valTypes) {
+            const testName = `${key.type}_${val.type}`.replaceAll(" ", "-");
+            const tactFilePath = await instantiateContractTemplateAndSpec(
+                templateTactSourceCode,
+                templateSpecSourceCode,
+                testName,
+                key,
+                val,
+            );
+            await run({
+                fileName: tactFilePath,
+                suppressLog: true,
+            });
+        }
+    }
+};
+
+const main = async () => {
+    try {
+        await compileContracts();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+void main();
--- src/test/e2e-emulated/map-tests/map-properties-key-value-types.ts
+++ src/test/e2e-emulated/map-tests/map-properties-key-value-types.ts
@@ -1,49 +1,49 @@
 export type MapType = {
     type: string;
-    val1: number;
-    val2: number;
+    val1: string;
+    val2: string;
 };
 
-const intSizes = [1, 42, 257];
-const uintSizes = [1, 32, 256];
+const intSizes = [8, 42, 257];
+const uintSizes = [9, 32, 256];
 
 const ints: MapType[] = intSizes.map((size) => ({
     type: `Int as int${size}`,
-    val1: -5,
-    val2: 42,
+    val1: "-5n",
+    val2: "42n",
 }));
 
 const uints: MapType[] = uintSizes.map((size) => ({
     type: `Int as uint${size}`,
-    val1: 6,
-    val2: 121,
+    val1: "6n",
+    val2: "121n",
 }));
 
 const varInts = [
     {
         type: "Int as varint16",
-        val1: -7,
-        val2: 1000,
+        val1: "-7n",
+        val2: "1000n",
     },
     {
         type: "Int as varint32",
-        val1: -74,
-        val2: 10000,
+        val1: "-74n",
+        val2: "10000n",
     },
     {
         type: "Int as varuint16",
-        val1: 7,
-        val2: 100,
+        val1: "7n",
+        val2: "100n",
     },
     {
         type: "Int as varuint32",
-        val1: 740,
-        val2: 100000,
+        val1: "740n",
+        val2: "100000n",
     },
     {
         type: "Int as coins",
-        val1: 7,
-        val2: 100,
+        val1: "7n",
+        val2: "100n",
     },
 ];
 
@@ -51,8 +51,8 @@ const allInts = ints.concat(uints, varInts);
 
 const address = {
     type: "Address",
-    val1: 4,
-    val2: 42,
+    val1: 'address("UQBKgXCNLPexWhs2L79kiARR1phGH1LwXxRbNsCFF9doczSI")',
+    val2: "new Address(0, Buffer.alloc(32, 0))",
 };
 
 const otherTypes: MapType[] = [];
@@ -72,8 +72,8 @@ const otherTypes = [
 ];
 */
 
-//export const keyTypes = ints.concat(uints, [address]);
-// export const valTypes = allInts.concat(otherTypes, [address]);
+export const keyTypes = ints.concat(uints, [address]);
+export const valTypes = allInts.concat(otherTypes, [address]);
 
-export const keyTypes = [address];
-export const valTypes = [address];
+// export const keyTypes = [address];
+// export const valTypes = [address];
