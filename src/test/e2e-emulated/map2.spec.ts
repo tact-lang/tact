5a0f9b0e220ff320d47a79983596439f0537afd5
Danil Ovchinnikov <danil42worker@gmail.com>
2025-01-25 19:50:51 +0300

    merged random generation functions for tests into one file

--- src/test/e2e-emulated/map2.spec.ts
+++ src/test/e2e-emulated/map2.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { randomAddress } from "../utils/randomAddress";
+import { randomAddress } from "../utils/randomUtils";
 import {
     MapTestContract,
     MapTestContract$Data,
---
+++ src/test/utils/random-utils.ts
@@ -0,0 +1,21 @@
+import Prando from "prando";
+import { KeyPair, keyPairFromSeed } from "@ton/crypto";
+import { Address } from "@ton/core";
+
+export function randomKey(seed: string): KeyPair {
+    const random = new Prando(seed);
+    const res = Buffer.alloc(32);
+    for (let i = 0; i < res.length; i++) {
+        res[i] = random.nextInt(0, 256);
+    }
+    return keyPairFromSeed(res);
+}
+
+export function randomAddress(workchain: number, seed: string): Address {
+    const random = new Prando(seed);
+    const hash = Buffer.alloc(32);
+    for (let i = 0; i < hash.length; i++) {
+        hash[i] = random.nextInt(0, 255);
+    }
+    return new Address(workchain, hash);
+}
