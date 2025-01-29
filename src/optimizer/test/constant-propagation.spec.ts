db5c7e3e737933810694e45f5cac3ddf592267fe
jeshecdom <jeshecdom@hotmail.com>
2025-01-29 04:09:24 +0100

    refactor: moved tests out of deprecaed folder.

--- src/optimizer/test/constant-propagation.spec.ts
+++ src/optimizer/test/constant-propagation.spec.ts
@@ -1,18 +1,18 @@
-import { getAstFactory } from "../../../ast/ast-helpers";
-import { featureEnable } from "../../../config/features";
-import { CompilerContext } from "../../../context/context";
-import { openContext } from "../../../context/store";
-import { getParser } from "../../../grammar";
-import { defaultParser } from "../../../grammar/grammar";
+import { getAstFactory } from "../../ast/ast-helpers";
+import { featureEnable } from "../../config/features";
+import { CompilerContext } from "../../context/context";
+import { openContext } from "../../context/store";
+import { getParser } from "../../grammar";
+import { defaultParser } from "../../grammar/grammar";
 import {
     getAllStaticFunctions,
     resolveDescriptors,
-} from "../../../types/resolveDescriptors";
-import { getAllExpressionTypes } from "../../../types/resolveExpression";
-import { resolveSignatures } from "../../../types/resolveSignatures";
-import { resolveStatements } from "../../../types/resolveStatements";
-import { loadCases } from "../../../utils/loadCases";
-import { constantPropagationAnalysis } from "../constant-propagation";
+} from "../../types/resolveDescriptors";
+import { getAllExpressionTypes } from "../../types/resolveExpression";
+import { resolveSignatures } from "../../types/resolveSignatures";
+import { resolveStatements } from "../../types/resolveStatements";
+import { loadCases } from "../../utils/loadCases";
+import { constantPropagationAnalysis } from "../deprecated/constant-propagation";
 
 describe("constant-propagation", () => {
     for (const r of loadCases(__dirname + "/success/")) {
