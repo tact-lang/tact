e3f6ea9b9e01fb4bce75c5b8adfdc50b96590571
verytactical <186486509+verytactical@users.noreply.github.com>
2025-01-31 14:08:28 +0400

    test: add fake language pragma

--- src/test/utils/all-in-folder.build.ts
+++ src/test/utils/all-in-folder.build.ts
@@ -1,38 +1,55 @@
 import { glob } from "glob";
 import { createVirtualFileSystem } from "../../vfs/createVirtualFileSystem";
-import { Options } from "../../config/parseConfig";
+import { Options, optionsSchema } from "../../config/parseConfig";
 import { basename, dirname, extname, join } from "path";
 import { createNodeFileSystem } from "../../vfs/createNodeFileSystem";
 import { Logger, LogLevel } from "../../context/logger";
 import { run } from "../../cli/tact";
 import files from "../../stdlib/stdlib";
+import { readFile } from "fs/promises";
 
 // node.js 20+ builtin
 const globSync = (globs: string[], options: { cwd: string }) => {
     return globs.flatMap((g) => glob.sync(g, options));
 };
 
-export const allInFolder = async (folder: string, globs: string[]) => {
+type Project = {
+    name: string;
+    path: string;
+    output: string;
+    options: Options;
+}
+
+const pragmaStart = "//@";
+
+export const allInFolder = async (
+    folder: string,
+    globs: string[],
+    options: Options = { debug: true }
+) => {
     try {
         const stdlib = createVirtualFileSystem("@stdlib", files);
 
         const contracts = globSync(globs, { cwd: folder });
 
-        const projects = contracts.map((contractPath) => {
-            const options: Options = { debug: true };
-            if (contractPath.includes("inline")) {
-                options.experimental = { inline: true };
+        const projects: Project[] = [];
+        for (const contractPath of contracts) {
+            const contractOptions = structuredClone(options);
+            const contractCode = await readFile(join(folder, contractPath), 'utf-8');
+            const [firstLine] = contractCode.split('\n');
+            if (contractCode.startsWith(pragmaStart) && firstLine) {
+                const pragmaOptions = optionsSchema.parse(
+                    JSON.parse(firstLine.substring(pragmaStart.length))
+                );
+                Object.assign(contractOptions, pragmaOptions);
             }
-            if (contractPath.includes("external")) {
-                options.external = true;
-            }
-            return {
+            projects.push({
                 name: basename(contractPath, extname(contractPath)),
                 path: contractPath,
                 output: join(dirname(contractPath), "output"),
                 options,
-            };
-        });
+            });
+        }
 
         const project = createNodeFileSystem(folder, false);
 
