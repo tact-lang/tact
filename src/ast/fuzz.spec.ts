cbaadcbc32ea99aa5dfc3589b7d2d07866ade8b1
xpyctumo <30053565+xpyctumo@users.noreply.github.com>
2025-01-31 19:04:42 +0300

    test: parser/printer fuzz tests for expressions (#1248)

---
+++ src/ast/fuzz.spec.ts
@@ -0,0 +1,34 @@
+import fc from "fast-check";
+import { getParser } from "../grammar";
+import { eqExpressions, getAstFactory } from "../ast/ast-helpers";
+import { diffAstObjects, randomAstExpression } from "./random.infra";
+import { prettyPrint } from "./ast-printer";
+
+describe("Pretty Print Expressions", () => {
+    const maxDepth = 4;
+    const parser = getParser(getAstFactory(), "new");
+
+    it(`should parse AstExpression`, () => {
+        fc.assert(
+            fc.property(randomAstExpression(maxDepth), (generatedAst) => {
+                const prettyBefore = prettyPrint(generatedAst);
+
+                const parsedAst = parser.parseExpression(prettyBefore);
+                const prettyAfter = prettyPrint(parsedAst);
+
+                expect(prettyBefore).toBe(prettyAfter);
+                const actual = eqExpressions(generatedAst, parsedAst);
+                if (!actual) {
+                    diffAstObjects(
+                        generatedAst,
+                        parsedAst,
+                        prettyBefore,
+                        prettyAfter,
+                    );
+                }
+                expect(actual).toBe(true);
+            }),
+            { seed: 1, numRuns: 5000 },
+        );
+    });
+});
