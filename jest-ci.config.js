d767f11ee7be467e33576a3e0dd0504f8be8a402
i582 <51853996+i582@users.noreply.github.com>
2025-02-07 11:25:24 +0400

    use testPathIgnorePatterns

--- jest-ci.config.js
+++ jest-ci.config.js
@@ -1,12 +1,15 @@
 const mainConfig = require("./jest.config");
 
-const exclude =
-    process.env.RUNNER_OS === "macOS"
-        ? ["src/test/e2e-emulated/map-tests/build/*"] // due to limited resources we do not want to run e2e tests for the maps
-        : [];
+const testPathIgnorePatterns =
+    process.env.RUNNER_OS === "macOS" // due to limited resources we do not want to run e2e tests for the maps
+        ? [
+              ...(mainConfig.testPathIgnorePatterns || []),
+              "src/test/e2e-emulated/map-tests/build/*",
+          ]
+        : mainConfig.testPathIgnorePatterns;
 
 module.exports = {
     ...mainConfig,
-    exclude,
+    testPathIgnorePatterns,
     maxWorkers: "2",
 };
