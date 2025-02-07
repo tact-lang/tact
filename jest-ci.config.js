b38920cf6e8727bf2df453cd599a1d5d9aaabf19
i582 <51853996+i582@users.noreply.github.com>
2025-02-07 11:38:30 +0400

    use `process.platform`

--- jest-ci.config.js
+++ jest-ci.config.js
@@ -1,7 +1,7 @@
 const mainConfig = require("./jest.config");
 
 const testPathIgnorePatterns =
-    process.env.RUNNER_OS === "macOS" // due to limited resources we do not want to run e2e tests for the maps
+    process.platform === "darwin" // due to limited resources we do not want to run e2e tests for the maps
         ? [
               ...(mainConfig.testPathIgnorePatterns || []),
               "src/test/e2e-emulated/map-tests/build/*",
