796ada4230af224ed960182c1c8866b540d468ad
i582 <51853996+i582@users.noreply.github.com>
2025-02-07 11:15:45 +0400

    ci: don't run e2e tests for the maps on macOS CI
    Fixes #1723

--- jest-ci.config.js
+++ jest-ci.config.js
@@ -1,6 +1,12 @@
 const mainConfig = require("./jest.config");
 
+const exclude =
+    process.env.RUNNER_OS === "macOS"
+        ? ["src/test/e2e-emulated/map-tests/build/*"] // due to limited resources we do not want to run e2e tests for the maps
+        : [];
+
 module.exports = {
     ...mainConfig,
+    exclude,
     maxWorkers: "2",
 };
