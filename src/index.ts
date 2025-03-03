3fdfcc3a564944c541b97cfc23dfd523d410f815
verytactical <186486509+verytactical@users.noreply.github.com>
2025-03-03 18:15:42 +0400

    fix(api): export all dependencies of misti from index.ts (#2196)

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,21 @@ export * from "./config/parseConfig";
 export { PackageFileFormat } from "./packaging/fileFormat";
 
 export { VirtualFileSystem } from "./vfs/VirtualFileSystem";
-
 export { createVirtualFileSystem } from "./vfs/createVirtualFileSystem";
 
 export * from "./browser";
 export * from "./context/logger";
 export * from "./error/errors";
 export { ItemOrigin } from "./imports/source";
+
+export * from "./ast/ast-printer";
+export * from "./ast/ast";
+export * from "./context/context";
+export * from "./context/store";
+export * from "./grammar/grammar";
+export * from "./optimizer/constEval";
+export * from "./pipeline/build";
+export * from "./pipeline/precompile";
+export * from "./stdlib/stdlib";
+export * from "./types/resolveDescriptors";
+export * from "./types/types";
