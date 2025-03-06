2a196fe2650e268c1f4d30358cb438c3eec3c1fe
verytactical <186486509+verytactical@users.noreply.github.com>
2025-03-06 15:10:21 +0400

    fix: verifier failure (#2093)

--- examples/contracts.build.ts
+++ examples/contracts.build.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { glob } from "glob";
-import { verify } from "./verify";
+import { verify } from "../src/verify";
 import { Logger } from "../src/context/logger";
 import { __DANGER__disableVersionNumber } from "../src/pipeline/version";
 import { allInFolder } from "../src/test/utils/all-in-folder.build";
