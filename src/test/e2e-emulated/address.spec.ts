5946f1ea70bcdd7923cda84bc0383fe19f8c6ff7
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-03-18 11:50:24 +0100

    tests

--- src/test/e2e-emulated/address.spec.ts
+++ src/test/e2e-emulated/address.spec.ts
@@ -43,4 +43,30 @@ describe("address", () => {
             "0:4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873",
         );
     });
+
+    it("should implement (de)serialization of BasechainAddresses correctly", async () => {
+        // Check the instantiation
+        const addr1 = await contract.getBasechainAddressFromHash(null);
+        const addr2 =
+            await contract.getBasechainAddressFromHash(
+                0x4a81708d2cf7b15a1b362fbf64880451d698461f52f05f145b36c08517d76873n,
+            );
+
+        const cell1 = await contract.getBasechainAddressStore(addr1);
+        const cell2 = await contract.getBasechainAddressStore(addr2);
+
+        // Check .skip...
+        await contract.getBasechainAddressSkip(cell1.asSlice());
+        await contract.getBasechainAddressSkip(cell2.asSlice());
+
+        // Check .load...
+        const loadedAddr1 = await contract.getBasechainAddressLoad(
+            cell1.asSlice(),
+        );
+        expect(addr1.hash === loadedAddr1.hash).toBe(true);
+        const loadedAddr2 = await contract.getBasechainAddressLoad(
+            cell2.asSlice(),
+        );
+        expect(addr2.hash === loadedAddr2.hash).toBe(true);
+    });
 });
