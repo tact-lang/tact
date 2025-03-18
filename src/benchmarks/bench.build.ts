e52dca5e3c5b1d7c63284bdf644ed08961b10c7d
b4cksl4sh <andrei.sh@ton.org>
2025-03-18 15:12:13 +0300

    Updated benches

--- src/benchmarks/bench.build.ts
+++ src/benchmarks/bench.build.ts
@@ -21,7 +21,7 @@ const main = async () => {
         safety: { nullChecks: false },
         optimizations: {
             alwaysSaveContractData: false,
-            internalExternalReceiversOutsideMethodsMap: "disable",
+            internalExternalReceiversOutsideMethodsMap: "explorers-compatible",
         },
     });
 };
