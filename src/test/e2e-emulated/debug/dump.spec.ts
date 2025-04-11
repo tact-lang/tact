99d9812e9f5c6d2f4f0f6fef623533860bb5d575
Anton Trunov <anton@tonstudio.io>
2025-04-11 15:19:24 +0400

    refactor dump spec using Philip's set-up

--- src/test/e2e-emulated/debug/dump.spec.ts
+++ src/test/e2e-emulated/debug/dump.spec.ts
@@ -1,36 +1,42 @@
 import { toNano } from "@ton/core";
-import type { SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { Blockchain } from "@ton/sandbox";
 import { Tester } from "./output/dump_Tester";
 import "@ton/test-utils";
+import { cached } from "@/test/utils/cache-state";
 
-describe("dump builtin", () => {
-    let blockchain: Blockchain;
-    let treasury: SandboxContract<TreasuryContract>;
-    let contract: SandboxContract<Tester>;
+const deployValue = toNano("1"); // `dump` is expensive
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        blockchain.verbosity.print = false;
-        treasury = await blockchain.treasury("treasury");
+const setup = async () => {
+    const blockchain = await Blockchain.create();
+    blockchain.verbosity.print = false;
+    const treasury = await blockchain.treasury("treasury");
 
-        contract = blockchain.openContract(await Tester.fromInit());
+    const contract = blockchain.openContract(await Tester.fromInit());
 
-        const deployResult = await contract.send(
-            treasury.getSender(),
-            { value: toNano("10") },
-            null,
-        );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: treasury.address,
-            to: contract.address,
-            success: true,
-            deploy: true,
-        });
+    const deployResult = await contract.send(
+        treasury.getSender(),
+        { value: deployValue },
+        null,
+    );
+    expect(deployResult.transactions).toHaveTransaction({
+        from: treasury.address,
+        to: contract.address,
+        success: true,
+        deploy: true,
     });
 
+    return {
+        blockchain,
+        treasury,
+        contract,
+    };
+};
+
+describe("dump", () => {
+    const state = cached(setup);
+
     it("should dump values correctly in a receiver", async () => {
+        const { contract, treasury } = await state.get();
         const result = await contract.send(
             treasury.getSender(),
             { value: toNano("10") },
