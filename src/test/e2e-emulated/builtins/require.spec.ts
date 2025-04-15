2570386e9f6a0683faa9a3fc76e19047f348b237
i582 <51853996+i582@users.noreply.github.com>
2025-04-15 23:47:34 +0400

    fixed review issues

--- src/test/e2e-emulated/builtins/require.spec.ts
+++ src/test/e2e-emulated/builtins/require.spec.ts
@@ -1,22 +1,26 @@
 import { toNano } from "@ton/core";
 import type { SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { Blockchain } from "@ton/sandbox";
-import { Test } from "./output/require_Test";
+import {
+    Test,
+    GLOBAL_ERROR_VOTING_ENDED,
+    Test_errors_backward,
+} from "./output/require_Test";
 import "@ton/test-utils";
 
 describe("require", () => {
     let blockchain: Blockchain;
-    let treasure: SandboxContract<TreasuryContract>;
+    let treasury: SandboxContract<TreasuryContract>;
     let contract: SandboxContract<Test>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         blockchain.verbosity.print = false;
-        treasure = await blockchain.treasury("treasure");
+        treasury = await blockchain.treasury("treasury");
         contract = blockchain.openContract(await Test.fromInit());
 
         const result = await contract.send(
-            treasure.getSender(),
+            treasury.getSender(),
             {
                 value: toNano("10"),
             },
@@ -24,7 +28,7 @@ describe("require", () => {
         );
 
         expect(result.transactions).toHaveTransaction({
-            from: treasure.address,
+            from: treasury.address,
             to: contract.address,
             success: true,
             deploy: true,
@@ -33,25 +37,25 @@ describe("require", () => {
 
     it("should throw correct error", async () => {
         const result = await contract.send(
-            treasure.getSender(),
+            treasury.getSender(),
             {
                 value: toNano("10"),
             },
             "self constant",
         );
         expect(result.transactions).toHaveTransaction({
-            exitCode: 59195,
+            exitCode: Test_errors_backward[Test.ERROR_VOTING_ENDED],
         });
 
         const result2 = await contract.send(
-            treasure.getSender(),
+            treasury.getSender(),
             {
                 value: toNano("10"),
             },
             "global constant",
         );
         expect(result2.transactions).toHaveTransaction({
-            exitCode: 33768,
+            exitCode: Test_errors_backward[GLOBAL_ERROR_VOTING_ENDED],
         });
     });
 });
