6d62f2ca9f2cbd6bca9687c706e46a73b0d06546
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-04-15 16:03:03 +0400

    feat: Tact source code formatter (#2768)
    - CLI: `tact-fmt`

---
+++ src/fmt/test/ignore-directives.spec.ts
@@ -0,0 +1,254 @@
+import { test } from "@/fmt/test/helpers";
+
+describe("ignore directive for statements", () => {
+    it(
+        "ignore let statement",
+        test(
+            `
+        fun foo() {
+            // fmt-ignore
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            // fmt-ignore
+            let   a =
+                100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore let statement with comment",
+        test(
+            `
+        fun foo() {
+            // fmt-ignore just for fun
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            // fmt-ignore just for fun
+            let   a =
+                100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore let statement without leading space",
+        test(
+            `
+        fun foo() {
+            //fmt-ignore
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            //fmt-ignore
+            let   a =
+                100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore let statement with other comment after directive",
+        test(
+            `
+        fun foo() {
+            // fmt-ignore
+            // why? idk
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            // fmt-ignore
+            // why? idk
+            let   a =
+                100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore let statement with extra newline after directive",
+        test(
+            `
+        fun foo() {
+            // fmt-ignore
+
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            // fmt-ignore
+
+            let a = 100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore let statement with block comment",
+        test(
+            `
+        fun foo() {
+            /* fmt-ignore */
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            /* fmt-ignore */
+            let   a =
+                100;
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore if statement",
+        test(
+            `
+        fun foo() {
+            // fmt-ignore
+            if (foo) {
+                    aaa =    10
+                    + 1;
+
+            }
+
+
+             else  {
+                if (  true   ) {}
+                    }
+
+            let   b    = 100;
+        }
+    `,
+            `
+        fun foo() {
+            // fmt-ignore
+            if (foo) {
+                    aaa =    10
+                    + 1;
+
+            }
+
+
+             else  {
+                if (  true   ) {}
+                    }
+
+            let b = 100;
+        }
+    `,
+        ),
+    );
+});
+
+describe("ignore directive for top levels", () => {
+    it(
+        "ignore function",
+        test(
+            `
+        // fmt-ignore
+        fun foo() {
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+
+        fun    bar ()  {  return 10; }
+    `,
+            `
+        // fmt-ignore
+        fun foo() {
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+
+        fun bar() {
+            return 10;
+        }
+    `,
+        ),
+    );
+
+    it(
+        "ignore function with /// comment",
+        test(
+            `
+        /// some doc
+        /// fmt-ignore
+        fun foo() {
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+
+        fun    bar ()  {  return 10; }
+    `,
+            `
+        /// some doc
+        /// fmt-ignore
+        fun foo() {
+            let   a =
+                100;
+
+            let   b    = 100;
+        }
+
+        fun bar() {
+            return 10;
+        }
+    `,
+        ),
+    );
+});
