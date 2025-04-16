bed3caa08f1041798df2ced434041c087b7f0509
i582 <51853996+i582@users.noreply.github.com>
2025-04-16 13:36:42 +0400

    fixed review issue

--- src/test/e2e-emulated/expressions/unary-plus.spec.ts
+++ src/test/e2e-emulated/expressions/unary-plus.spec.ts
@@ -1,36 +1,60 @@
 import { toNano } from "@ton/core";
-import type { SandboxContract, TreasuryContract } from "@ton/sandbox";
 import { Blockchain } from "@ton/sandbox";
 import { TestContract } from "./output/unary-plus_TestContract";
 import "@ton/test-utils";
+import { cached } from "@/test/utils/cache-state";
+
+const deployValue = toNano("1");
+
+const setup = async () => {
+    const blockchain = await Blockchain.create();
+    blockchain.verbosity.print = false;
+    const treasury = await blockchain.treasury("treasury");
+
+    const contract = blockchain.openContract(await TestContract.fromInit());
+
+    const deployResult = await contract.send(
+        treasury.getSender(),
+        { value: deployValue },
+        null,
+    );
+    expect(deployResult.transactions).toHaveTransaction({
+        from: treasury.address,
+        to: contract.address,
+        success: true,
+        deploy: true,
+    });
+
+    return {
+        blockchain,
+        treasury,
+        contract,
+    };
+};
 
 describe("unary-plus", () => {
-    let blockchain: Blockchain;
-    let treasure: SandboxContract<TreasuryContract>;
-    let contract: SandboxContract<TestContract>;
+    const state = cached(setup);
 
     beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        blockchain.verbosity.print = false;
-        treasure = await blockchain.treasury("treasure");
-
-        contract = blockchain.openContract(await TestContract.fromInit());
+        const { contract, treasury } = await state.get();
 
         const deployResult = await contract.send(
-            treasure.getSender(),
+            treasury.getSender(),
             { value: toNano("10") },
             null,
         );
 
         expect(deployResult.transactions).toHaveTransaction({
-            from: treasure.address,
+            from: treasury.address,
             to: contract.address,
             success: true,
-            deploy: true,
         });
     });
 
     it("should implement unary plus operator correctly", async () => {
+        const { contract } = await state.get();
         expect(await contract.getFoo()).toEqual(2n);
+        expect(await contract.getFoo1()).toEqual(2n);
+        expect(await contract.getFoo2()).toEqual(-1n);
     });
 });
