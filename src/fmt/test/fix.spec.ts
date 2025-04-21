ca1369bcee104c52440af9d89629ea2392f9d2f0
i582 <51853996+i582@users.noreply.github.com>
2025-04-21 19:29:02 +0400

    fmt

--- src/fmt/test/fix.spec.ts
+++ src/fmt/test/fix.spec.ts
@@ -5,27 +5,27 @@ describe("formatter automatic fixes", () => {
         "should format Foo { value: value } as Foo { value }",
         test(
             `
-            fun foo() {
-                Foo { value: value };
-                Foo { value: value2 };
-                Foo { value: msg.value };
-                Foo { value: value() };
-                Foo { value: 10 };
-                Foo { value: value, other: other };
-                Foo { value: value, other: other2 };
-            }
-        `,
+                fun foo() {
+                    Foo { value: value };
+                    Foo { value: value2 };
+                    Foo { value: msg.value };
+                    Foo { value: value() };
+                    Foo { value: 10 };
+                    Foo { value: value, other: other };
+                    Foo { value: value, other: other2 };
+                }
+            `,
             `
-            fun foo() {
-                Foo { value };
-                Foo { value: value2 };
-                Foo { value: msg.value };
-                Foo { value: value() };
-                Foo { value: 10 };
-                Foo { value, other };
-                Foo { value, other: other2 };
-            }
-        `,
+                fun foo() {
+                    Foo { value };
+                    Foo { value: value2 };
+                    Foo { value: msg.value };
+                    Foo { value: value() };
+                    Foo { value: 10 };
+                    Foo { value, other };
+                    Foo { value, other: other2 };
+                }
+            `,
         ),
     );
 });
