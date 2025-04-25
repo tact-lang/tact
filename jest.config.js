d6e30cabf8febf8831c24f11cb47b91277949d7a
xpyctumo <30053565+xpyctumo@users.noreply.github.com>
2025-04-25 19:03:28 +0300

    chore: add `yarn test:allure` command

--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,7 @@ module.exports = {
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     globalSetup: "./jest.globalSetup.js",
     setupFiles: ["./jest.setup.js"],
+    setupFilesAfterEnv: ["./src/test/allure/jest.allure.ts"],
     globalTeardown: "./jest.teardown.js",
     snapshotSerializers: ["@tact-lang/ton-jest/serializers"],
     maxWorkers: "4",
--- src/test/utils/write-vm-log.ts
+++ src/test/utils/write-vm-log.ts
@@ -1,4 +1,5 @@
 import type { Blockchain } from "@ton/sandbox";
+import * as allure from "allure-js-commons";
 import { appendFileSync, mkdirSync, rmSync, writeFileSync } from "fs";
 import { dirname } from "path";
 
@@ -16,31 +17,33 @@ export const writeLog = ({ blockchain, path }: WriteLogParams) => {
     }
     writeFileSync(path, "");
     return async function step<T>(name: string, callback: () => Promise<T>) {
-        const oldLog = console.log;
-        const oldVmLogs = blockchain.verbosity.vmLogs;
-        const oldBcLogs = blockchain.verbosity.blockchainLogs;
-        try {
-            console.log = (message: unknown, ...rest: unknown[]) => {
-                if (typeof message !== "string" || rest.length > 0) {
-                    throw new Error("Unexpected log");
-                }
-                const lines = message
-                    .split("\n")
-                    .map((s) => "    " + s)
-                    .join("\n");
-                const entry = `  - |\n${lines}\n`;
+        return await allure.step(name, async () => {
+            const oldLog = console.log;
+            const oldVmLogs = blockchain.verbosity.vmLogs;
+            const oldBcLogs = blockchain.verbosity.blockchainLogs;
+            try {
+                console.log = (message: unknown, ...rest: unknown[]) => {
+                    if (typeof message !== "string" || rest.length > 0) {
+                        throw new Error("Unexpected log");
+                    }
+                    const lines = message
+                        .split("\n")
+                        .map((s) => "    " + s)
+                        .join("\n");
+                    const entry = `  - |\n${lines}\n`;
+                    appendFileSync(path, entry);
+                };
+                blockchain.verbosity.vmLogs = "vm_logs_full";
+                blockchain.verbosity.blockchainLogs = true;
+                const entry = `- name: ${name}\n  messages:\n`;
                 appendFileSync(path, entry);
-            };
-            blockchain.verbosity.vmLogs = "vm_logs_full";
-            blockchain.verbosity.blockchainLogs = true;
-            const entry = `- name: ${name}\n  messages:\n`;
-            appendFileSync(path, entry);
-            return await callback();
-        } finally {
-            console.log = oldLog;
-            blockchain.verbosity.vmLogs = oldVmLogs;
-            blockchain.verbosity.blockchainLogs = oldBcLogs;
-        }
+                return await callback();
+            } finally {
+                console.log = oldLog;
+                blockchain.verbosity.vmLogs = oldVmLogs;
+                blockchain.verbosity.blockchainLogs = oldBcLogs;
+            }
+        });
     };
 };
 
