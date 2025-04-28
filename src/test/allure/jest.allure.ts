c873bdba913c476b4ae80989cc7cf60e7fa5c4d6
xpyctumo <30053565+xpyctumo@users.noreply.github.com>
2025-04-28 14:48:14 +0300

    fix: formatter

--- src/test/allure/jest.allure.ts
+++ src/test/allure/jest.allure.ts
@@ -1,6 +1,6 @@
 import { inspect } from "util";
 import * as allure from "allure-js-commons";
-import {Blockchain} from "@ton/sandbox";
+import { Blockchain } from "@ton/sandbox";
 
 declare global {
     interface BigInt {
@@ -8,33 +8,33 @@ declare global {
     }
 }
 
-BigInt.prototype.toJSON = function () { return Number(this) }
+BigInt.prototype.toJSON = function () {
+    return Number(this);
+};
 const originalCreate: typeof Blockchain.create = Blockchain.create;
 
-Blockchain.create = (async function proxy(
+Blockchain.create = async function proxy(
     this: typeof Blockchain,
     ...args: Parameters<typeof Blockchain.create>
 ) {
     return allure.step("Blockchain.create()", () =>
-        originalCreate.apply(this, args)
+        originalCreate.apply(this, args),
     );
-}) as typeof Blockchain.create;
+} as typeof Blockchain.create;
 
-const originalTreasury: typeof Blockchain.prototype.treasury = Blockchain.prototype.treasury;
+const originalTreasury: typeof Blockchain.prototype.treasury =
+    Blockchain.prototype.treasury;
 
 Blockchain.prototype.treasury = async function proxyTreasury(
     this: Blockchain,
     ...args: Parameters<typeof originalTreasury>
 ) {
-    const argStr = args
-        .map(safeString)
-        .join(", ");
+    const argStr = args.map(safeString).join(", ");
 
     const label = `Blockchain.treasury(${argStr})`;
 
     return await allure.step(label, () => originalTreasury.apply(this, args));
 } as typeof originalTreasury;
-
 
 // const originalOpenContract: typeof Blockchain.prototype.openContract =
 //     Blockchain.prototype.openContract;
@@ -54,13 +54,13 @@ Blockchain.prototype.treasury = async function proxyTreasury(
 
 function safeString(v: unknown): string {
     if (v === null || v === undefined) return String(v);
-    if (typeof v === "string" || typeof v === "number" || typeof v === "boolean") {
+    if (
+        typeof v === "string" ||
+        typeof v === "number" ||
+        typeof v === "boolean"
+    ) {
         return JSON.stringify(v);
     }
 
     return inspect(v, { depth: 1, breakLength: 50 });
 }
-
-
-
-
