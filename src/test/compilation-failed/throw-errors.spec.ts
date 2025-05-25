a33bd2219f1ae389ca0b00a108f9ca6342737889
Anton Trunov <anton@tonstudio.io>
2025-05-25 13:59:35 +0400

    test with negative exit code

--- src/test/compilation-failed/throw-errors.spec.ts
+++ src/test/compilation-failed/throw-errors.spec.ts
@@ -2,8 +2,13 @@ import { itShouldNotCompile } from "@/test/compilation-failed/util";
 
 describe("throw-errors", () => {
     itShouldNotCompile({
-        testName: "throw-in-fallback-receiver",
+        testName: "throw-in-fallback-receiver-large-positive",
         errorMessage:
             'Invalid exit code for "throw": 57896044618658097711785492504343953926634992332820282019728792003956564819968, but it must be in range [0, 65535]',
+    });
+    itShouldNotCompile({
+        testName: "throw-in-fallback-receiver-negative",
+        errorMessage:
+            'Invalid exit code for "throw": -14, but it must be in range [0, 65535]',
     });
 });
