024a3212e1cbf2b02ce32226cf22a2993716a297
i582 <51853996+i582@users.noreply.github.com>
2025-05-26 12:32:48 +0400

    add tests with comparison and mutations

---
+++ src/test/e2e-emulated/stdlib/comparisons.spec.ts
@@ -0,0 +1,47 @@
+import { toNano } from "@ton/core";
+import { Blockchain } from "@ton/sandbox";
+import { ComparisonsTester } from "./output/comparisons_ComparisonsTester";
+import "@ton/test-utils";
+import { cached } from "@/test/utils/cache-state";
+
+const deployValue = toNano("1");
+
+const setup = async () => {
+    const blockchain = await Blockchain.create();
+    blockchain.verbosity.print = false;
+
+    const treasury = await blockchain.treasury("treasury");
+    const contract = blockchain.openContract(
+        await ComparisonsTester.fromInit(),
+    );
+
+    const deployResult = await contract.send(
+        treasury.getSender(),
+        { value: deployValue },
+        null,
+    );
+    expect(deployResult.transactions).toHaveTransaction({
+        from: treasury.address,
+        to: contract.address,
+        success: true,
+        deploy: true,
+    });
+
+    return {
+        blockchain,
+        treasury,
+        contract,
+    };
+};
+
+describe("comparisons", () => {
+    const state = cached(setup);
+
+    it("should perform comparisons with mutation correctly", async () => {
+        const { contract } = await state.get();
+
+        expect(await contract.getCompare1(10n)).toBe(false);
+        expect(await contract.getCompare2(10n)).toBe(false);
+        expect(await contract.getCompare3(10n)).toBe(true);
+    });
+});
