0649be1ff3d530bcde018e66928a59bdf047aef7
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-05-26 16:40:31 +0400

    fix: allow `bounced<T>` as a return type of assembly functions (#3259)

--- src/types/resolveExpression.ts
+++ src/types/resolveExpression.ts
@@ -520,7 +520,7 @@ function resolveFieldAccess(
     if (src.kind === "ref_bounced" && field) {
         if (field.type.kind === "map") {
             throwCompilationError(
-                `Cannot access field with map type inside bounced receiver`,
+                `Cannot access field of map type from bounced<${src.name}>`,
                 exp.field.loc,
             );
         }
@@ -530,7 +530,7 @@ function resolveFieldAccess(
             if (name === "Cell" || name === "Slice" || name === "Builder") {
                 const optional = field.type.optional ? "?" : "";
                 throwCompilationError(
-                    `Cannot access field with ${idTextErr(name + optional)} type inside bounced receiver`,
+                    `Cannot access field of ${idTextErr(name + optional)} type from bounced<${src.name}>`,
                     exp.field.loc,
                 );
             }
